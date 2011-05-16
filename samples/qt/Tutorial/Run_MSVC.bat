18473fc803fe432a85bd811dcca12110e4abd691
bugmaster <>
2011-05-16 13:52:57 +0000

    Update for VS installed by default

--- samples/qt/Tutorial/Run_MSVC.bat
+++ samples/qt/Tutorial/Run_MSVC.bat
@@ -11,12 +11,13 @@ rem Define path to project file
 set PRJFILE=%~dp0Tutorial.sln
 
 rem Launch Visual Studio - either professional (devenv) or Express, as available
-if exist %DevEnvDir%\devenv.exe (
-    start %DevEnvDir%\devenv.exe %PRJFILE% /useenv
-) else if exist %DevEnvDir%\VCExpress.exe (
-    start %DevEnvDir%\VCExpress.exe %PRJFILE% /useenv
+if exist "%DevEnvDir%\devenv.exe"  (
+    start devenv.exe "%PRJFILE%" /useenv
+) else if exist "%DevEnvDir%\VCExpress.exe"  (
+    start VCExpress.exe "%PRJFILE%" /useenv
 ) else (
     echo Error: Could not find MS Visual Studio ^(%VCVER%^)
     echo Check relevant environment variable ^(e.g. VS80COMNTOOLS for vc8^)
 )
+
 
