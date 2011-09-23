94023077f206f33b5f52f0234f477c0186024cb4
bugmaster <>
2011-09-23 12:09:01 +0000

    Update OCCT version up to 6.5.2

--- src/Standard/Standard_Version.hxx
+++ src/Standard/Standard_Version.hxx
@@ -23,8 +23,8 @@
 // Primary definitions
 #define OCC_VERSION_MAJOR         6
 #define OCC_VERSION_MINOR         5
-#define OCC_VERSION_MAINTENANCE   1
-#define OCC_VERSION_DEVELOPMENT // This line must be commented in any final version (release)
+#define OCC_VERSION_MAINTENANCE   2
+//#define OCC_VERSION_DEVELOPMENT // This line must be commented in any final version (release)
 
 // Derived: version as real and string (major.minor)
 #define OCC_VERSION         6.5
