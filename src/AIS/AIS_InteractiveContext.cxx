cb15fdf727dba69b405bc4f1a1a903d70ebeb870
Pawel <pawel-kowalski@wp.pl>
2012-04-16 15:12:07 +0200

    0022903: DeviationAngle from AIS_InteractiveContext returns constant value Returning myDefaultDrawer->DeviationAngle() instead of a constant value

--- src/AIS/AIS_InteractiveContext.cxx
+++ src/AIS/AIS_InteractiveContext.cxx
@@ -1661,8 +1661,8 @@ void AIS_InteractiveContext::SetDeviationAngle(const Standard_Real anAngle)
 
 Standard_Real AIS_InteractiveContext::DeviationAngle() const
 {
-  return M_PI/180.0e0 ;
-  //  return myDefaultDrawer->DeviationAngle();
+  //return M_PI/180.0e0 ;
+    return myDefaultDrawer->DeviationAngle();
 }
 
 //=======================================================================
