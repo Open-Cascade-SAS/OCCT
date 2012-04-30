9220e7851aacae79ea9b7727e398eb1551407c50
bugmaster <bugmaster@opencascade.com>
2012-04-30 14:12:13 +0400

    0023124: Porting products samples on OCCT 6.5.3 Removed redundant scripts from samples/qt/Common and samples/qt/Interface folders Fixed Linux scripts for Tutorial and ImportExport samples

--- samples/qt/IESample/run.sh
+++ samples/qt/IESample/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export aSamplePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -e "${aSamplePath}/custom.sh" ]; then source "${aSamplePath}/env.sh"; fi
+if [ -e "${aSamplePath}/env.sh" ]; then source "${aSamplePath}/env.sh"; fi
 cd $aSamplePath
 
 host=`uname -s`
