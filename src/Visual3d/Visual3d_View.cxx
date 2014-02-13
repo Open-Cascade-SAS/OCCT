ac29779aa95c07725e838ee04b0c517ba7fb089d
aba <aba@opencascade.com>
2014-02-13 13:09:42 +0400

    0024358: TKV3d - connected structures are not re-computed on device lost
    Graphic3d_StructureManager::ReComputeStructures() method was corrected to consider all child (connected) structures.
    Corrected remarks: added image dump in test case; corrected method name.

--- src/Visual3d/Visual3d_View.cxx
+++ src/Visual3d/Visual3d_View.cxx
@@ -1671,7 +1671,7 @@ void Visual3d_View::Update (const Handle(Visual3d_Layer)& AnUnderLayer, const Ha
 
         if (MyGraphicDriver->IsDeviceLost())
         {
-          MyViewManager->ReComputeStructures();
+          MyViewManager->RecomputeStructures();
           MyGraphicDriver->ResetDeviceLostFlag();
         }
 
