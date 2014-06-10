acbb658afeb2364e1fc1994a81e540d8600a3fa7
razmyslovich <razmyslovich@volumegraphics.com>
2014-06-10 13:50:14 +0200

    0024998: The incorrect _USE_MATH_DEFINES definition in Standard_math.hxx prevents the clean build for client applications
    Add #ifndef clause to avoid redefinition compile error

--- src/Standard/Standard_math.hxx
+++ src/Standard/Standard_math.hxx
@@ -18,7 +18,10 @@
 
 #ifdef _MSC_VER
 
+#ifndef _USE_MATH_DEFINES
 #define _USE_MATH_DEFINES
+#endif
+
 #include <math.h>
 
 // MSVC versions prior to 12 did not provided acosh, asinh, atanh functions in standard library
