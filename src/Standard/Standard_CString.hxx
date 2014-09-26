868dbdbae43dbcaf6caa3344cf0ef22073ceb5f7
abv <abv@opencascade.com>
2014-09-26 12:48:04 +0400

    0025005: Global function HashCodes() is not exported on Windows
    HashCodes() is exported

--- src/Standard/Standard_CString.hxx
+++ src/Standard/Standard_CString.hxx
@@ -53,8 +53,8 @@ inline Standard_Integer HashCode (const Standard_CString,
                                   const Standard_Integer ,
                                   const Standard_Integer ,
                                   Standard_Integer& );
-Standard_Integer HashCodes (const Standard_CString ,
-                            const Standard_Integer  );
+__Standard_API Standard_Integer HashCodes (const Standard_CString,
+                                           const Standard_Integer);
 
 //! Equivalents of functions from standard C library that use always C locale
 __Standard_API double Atof    (const char* theStr);
