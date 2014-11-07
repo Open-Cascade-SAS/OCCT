32d5cd7b98afe6c5d0adea6842eddf4265d99f33
bugmaster <bugmaster@opencascade.com>
2014-11-07 17:56:30 +0300

    Incrementation of OCCT version up to 6.8.0

--- src/Standard/Standard_Version.hxx
+++ src/Standard/Standard_Version.hxx
@@ -42,7 +42,7 @@
 //! - "dev" for official (certified) version (master branch) between releases
 //! - "beta..." or "rc..." for beta releases or release candidates
 //! - "project..." for version containing project-specific fixes
-#define OCC_VERSION_DEVELOPMENT   "beta"
+//#define OCC_VERSION_DEVELOPMENT   "beta"
 
 // Derived (manually): version as real and string (major.minor)
 #define OCC_VERSION               6.8
