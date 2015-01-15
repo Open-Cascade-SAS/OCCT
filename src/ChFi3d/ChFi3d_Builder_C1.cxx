6504f8cd28a1797e8e5de611b08c3c3c39182320
jgv <jgv@opencascade.com>
2015-01-15 15:04:05 +0300

    0025657: Bad result of Fillet operation
    Comments have been precised.
    
    Test case for issue CR25657
    
    Fix of regression
    
    Additional test case.

--- src/ChFi3d/ChFi3d_Builder_C1.cxx
+++ src/ChFi3d/ChFi3d_Builder_C1.cxx
@@ -371,29 +371,30 @@ static Standard_Boolean Update(Handle(Adaptor3d_HSurface)& face,
     pared = ponc1.Parameter();
     parltg = ponc2.Parameter();
     if ((parltg > f) && (parltg < l)) {
-#ifdef OCC23139
-      ////modified by jgv, 10.05.2012 for the bug 23139////
+      ////modified by jgv, 10.05.2012 for the bug 23139, 25657////
       Handle(Geom2d_Curve) PConF = fi.PCurveOnFace();
       if (!PConF.IsNull())
       {
         Handle(Geom2d_TrimmedCurve) aTrCurve = Handle(Geom2d_TrimmedCurve)::DownCast(PConF);
         if (!aTrCurve.IsNull())
           PConF = aTrCurve->BasisCurve();
-        if (isfirst)
+        if (!PConF->IsPeriodic())
         {
-          Standard_Real fpar = PConF->FirstParameter();
-          if (parltg < fpar)
-            parltg = fpar;
-        }
-        else
-        {
-          Standard_Real lpar = PConF->LastParameter();
-          if (parltg > lpar)
-            parltg = lpar;
+          if (isfirst)
+          {
+            Standard_Real fpar = PConF->FirstParameter();
+            if (parltg < fpar)
+              parltg = fpar;
+          }
+          else
+          {
+            Standard_Real lpar = PConF->LastParameter();
+            if (parltg > lpar)
+              parltg = lpar;
+          }
         }
       }
       /////////////////////////////////////////////////////
-#endif
       fi.SetParameter(parltg,isfirst);
       cp.SetArc(cp.Tolerance(),cp.Arc(),pared,cp.TransitionOnArc());
       return Standard_True;
