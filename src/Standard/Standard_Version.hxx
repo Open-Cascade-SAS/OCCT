cbb329c42cb7c072d8996190dcbe70af08325667
vsr <vsr@opencascade.com>
2015-06-10 15:18:49 +0300

    Increment OCCT version up to 6.9.1dev

--- src/Standard/Standard_Version.hxx
+++ src/Standard/Standard_Version.hxx
@@ -35,19 +35,19 @@
 // Primary definitions
 #define OCC_VERSION_MAJOR         6
 #define OCC_VERSION_MINOR         9
-#define OCC_VERSION_MAINTENANCE   0
+#define OCC_VERSION_MAINTENANCE   1
 
 //! This macro must be commented in official release, and set to non-empty 
 //! string in other situations, to identify specifics of the version, e.g.:
 //! - "dev" for official (certified) version (master branch) between releases
 //! - "beta..." or "rc..." for beta releases or release candidates
 //! - "project..." for version containing project-specific fixes
-//#define OCC_VERSION_DEVELOPMENT   "beta"
+#define OCC_VERSION_DEVELOPMENT   "dev"
 
 // Derived (manually): version as real and string (major.minor)
 #define OCC_VERSION               6.9
 #define OCC_VERSION_STRING       "6.9"
-#define OCC_VERSION_COMPLETE     "6.9.0"
+#define OCC_VERSION_COMPLETE     "6.9.1"
 
 //! Derived: extended version as string ("major.minor.maintenance.dev")
 #ifdef OCC_VERSION_DEVELOPMENT
