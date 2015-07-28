a447178e3f8cb52b3af5f65976f1572f70f4aa7a
duv <duv@opencascade.com>
2015-07-28 16:17:57 +0300

    0026492: OpenGl_FrameBuffer does not releases itself correctly
    Logic related to myIsOwnBuffer flag in OpenGl_FrameBuffer was corrected.

--- src/OpenGl/OpenGl_FrameBuffer.cxx
+++ src/OpenGl/OpenGl_FrameBuffer.cxx
@@ -62,6 +62,8 @@ Standard_Boolean OpenGl_FrameBuffer::Init (const Handle(OpenGl_Context)& theGlCo
   // clean up previous state
   Release (theGlContext.operator->());
 
+  myIsOwnBuffer = true;
+
   // setup viewport sizes as is
   myVPSizeX = theSizeX;
   myVPSizeY = theSizeY;
@@ -163,6 +165,8 @@ Standard_Boolean OpenGl_FrameBuffer::InitWithRB (const Handle(OpenGl_Context)& t
 
   // clean up previous state
   Release (theGlCtx.operator->());
+
+  myIsOwnBuffer = true;
 
   // setup viewport sizes as is
   myVPSizeX = theSizeX;
