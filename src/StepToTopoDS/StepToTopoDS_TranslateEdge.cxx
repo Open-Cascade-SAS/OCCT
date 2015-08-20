82314bdf8c6f6a35ee2545a2b2e6f8f4e33783a2
gka <gka@opencascade.com>
2015-08-20 16:53:50 +0300

    0026289: STEP import/export produces an empty shape
    Exception is raised during reading step file containing p-curve having not-unique last knot.
    Modification correcting misprint during computation new numbers of poles for such case was made in the file StepToGeom_MakeBSplineCurve.pxx
    Protection against exception was made in the file StepToTopoDS/StepToTopoDS_TranslateEdge.cxx.
    Warning message was added in the file StepToTopoDS_TranslateEdgeLoop.cxx.
    
    Test case for issue CR26289

--- src/StepToTopoDS/StepToTopoDS_TranslateEdge.cxx
+++ src/StepToTopoDS/StepToTopoDS_TranslateEdge.cxx
@@ -61,6 +61,7 @@
 #include <TopoDS_Vertex.hxx>
 #include <Transfer_TransientProcess.hxx>
 #include <UnitsMethods.hxx>
+#include <Standard_Failure.hxx>
 
 //#include <StepGeom_Polyline.hxx>
 //#include <TransferBRep.hxx>
@@ -501,11 +502,19 @@ Handle(Geom2d_Curve)  StepToTopoDS_TranslateEdge::MakePCurve
   const Handle(StepRepr_DefinitionalRepresentation) DRI = PCU->ReferenceToCurve();
   if( DRI.IsNull()) return C2d;
   const Handle(StepGeom_Curve) StepCurve = Handle(StepGeom_Curve)::DownCast(DRI->ItemsValue(1));
-  C2d = StepToGeom::MakeCurve2d (StepCurve);
-  if (! C2d.IsNull()) {
+  try
+  {
+    C2d = StepToGeom::MakeCurve2d (StepCurve);
+    if (! C2d.IsNull()) {
     // -- if the surface is a RectangularTrimmedSurface, 
     // -- send the BasisSurface.
-    C2d = UnitsMethods::DegreeToRadian(C2d, ConvSurf);
+     C2d = UnitsMethods::DegreeToRadian(C2d, ConvSurf);
+    }
+    
+  }
+  catch(Standard_Failure)
+  {
+    return C2d;
   }
   return C2d;
 }
