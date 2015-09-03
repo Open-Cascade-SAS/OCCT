c3a17136720a0c51500c8b09258e262b01837ad7
ika <ika@opencascade.com>
2015-09-03 14:36:27 +0300

    0026451: Crash importing STeP file
    Add check for NULL.
    
    Test case for issue CR26451

--- src/StepShape/StepShape_OrientedEdge.cxx
+++ src/StepShape/StepShape_OrientedEdge.cxx
@@ -65,6 +65,8 @@ Handle(StepShape_Vertex) StepShape_OrientedEdge::EdgeStart() const
 {
 	// WARNING : the field is redefined.
 	// method body is not yet automaticly wrote
+  if (edgeElement.IsNull())
+    return NULL;
   if (Orientation()) {
     return edgeElement->EdgeStart();
   }
@@ -84,7 +86,8 @@ Handle(StepShape_Vertex) StepShape_OrientedEdge::EdgeEnd() const
 {
 	// WARNING : the field is redefined.
 	// method body is not yet automaticly wrote
-
+  if (edgeElement.IsNull())
+    return NULL;
   if (Orientation()) {
     return edgeElement->EdgeEnd();
   }
