6e448ab08126b00486bcb1de00596cf3a9640e30
abv <abv@opencascade.com>
2016-02-01 17:33:25 +0300

    0026415: <TFunction_GraphNode> XML should not break line in the middle of text.
    Line break between "previous" and "next" ids is replaced by additional spaces (these facilitate visual separation of the two sub-lists).

--- src/XmlMFunction/XmlMFunction_GraphNodeDriver.cxx
+++ src/XmlMFunction/XmlMFunction_GraphNodeDriver.cxx
@@ -184,7 +184,10 @@ void XmlMFunction_GraphNodeDriver::Paste (const Handle(TDF_Attribute)& theSource
     aValueStr += TCollection_AsciiString(ID);
     aValueStr += ' ';
   }
-  aValueStr += "\n";
+
+  // add more spaces between "previous" and "next" ids to make them easily
+  // recognizable for human
+  aValueStr += "   ";
 
   // Next
   // ====
