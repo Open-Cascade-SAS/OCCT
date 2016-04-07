6d4bf6d9b1a2e52ae2e9fe7bb810c382166c60ee
bugmaster <bugmaster@opencascade.com>
2016-04-07 17:50:11 +0300

    Incrementation of OCCT version up to 7.0.1

--- src/Standard/Standard_Version.hxx
+++ src/Standard/Standard_Version.hxx
@@ -35,19 +35,19 @@
 // Primary definitions
 #define OCC_VERSION_MAJOR         7
 #define OCC_VERSION_MINOR         0
-#define OCC_VERSION_MAINTENANCE   0
+#define OCC_VERSION_MAINTENANCE   1
 
 //! This macro must be commented in official release, and set to non-empty 
 //! string in other situations, to identify specifics of the version, e.g.:
 //! - "dev" for official (certified) version (master branch) between releases
 //! - "beta..." or "rc..." for beta releases or release candidates
 //! - "project..." for version containing project-specific fixes
-//#define OCC_VERSION_DEVELOPMENT   "rc"
+#define OCC_VERSION_DEVELOPMENT   "dev"
 
 // Derived (manually): version as real and string (major.minor)
 #define OCC_VERSION               7.0
 #define OCC_VERSION_STRING       "7.0"
-#define OCC_VERSION_COMPLETE     "7.0.0"
+#define OCC_VERSION_COMPLETE     "7.0.1"
 
 //! Derived: extended version as string ("major.minor.maintenance.dev")
 #ifdef OCC_VERSION_DEVELOPMENT
