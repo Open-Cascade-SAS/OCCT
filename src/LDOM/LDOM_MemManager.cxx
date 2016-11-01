579f2938a85ba9be301e3e55e9123cf68c50dcf8
pdn <pdn@opencascade.com>
2016-11-01 13:43:56 +0300

    0028034: Application Framework - stack overflow in LDOM destructor

--- src/LDOM/LDOM_MemManager.cxx
+++ src/LDOM/LDOM_MemManager.cxx
@@ -99,7 +99,14 @@ void * LDOM_MemManager::MemBlock::AllocateAndCheck
 LDOM_MemManager::MemBlock::~MemBlock ()
 {
   delete [] myBlock;
-  delete myNext;
+  MemBlock* aNext = myNext;
+  while (aNext) 
+  {
+    MemBlock* aNextNext = aNext->myNext;
+    aNext->myNext = 0;
+    delete aNext;
+    aNext = aNextNext;
+  }
 }
 
 //=======================================================================
