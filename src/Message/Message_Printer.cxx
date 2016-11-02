baed6838240054606f7b21be1a70af9152e3d5ca
kgv <kgv@opencascade.com>
2016-11-02 12:56:30 +0300

    0028040: Foundation Classes, Message_Printer - handle Standard_CString messages as UTF-8

--- src/Message/Message_Printer.cxx
+++ src/Message/Message_Printer.cxx
@@ -13,8 +13,8 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-
 #include <Message_Printer.hxx>
+
 #include <Standard_Type.hxx>
 #include <TCollection_AsciiString.hxx>
 #include <TCollection_ExtendedString.hxx>
@@ -41,7 +41,7 @@ void Message_Printer::Send (const Standard_CString theString,
 {
   if (theGravity >= myTraceLevel)
   {
-    Send (TCollection_ExtendedString (theString), theGravity, theToOutEol);
+    Send (TCollection_ExtendedString (theString, Standard_True), theGravity, theToOutEol);
   }
 }
 
