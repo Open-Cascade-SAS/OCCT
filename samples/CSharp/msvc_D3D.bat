da67ffb75a6ca1dc268d5d0fc2c0f1d064c6ee6e
apn <apn@opencascade.com>
2017-05-30 17:27:41 +0300

    0028769: Unification of msvc.bat files.
    Modify mfc and qt samples for supporting VS 2017.
    Create general vcxproj files for all mfc samples.
    
    Correction msvc.bat and msvc_D3D.bat in CSharp sample

--- samples/CSharp/msvc_D3D.bat
+++ samples/CSharp/msvc_D3D.bat
@@ -19,26 +19,6 @@ if NOT DEFINED DXSDK_DIR (
 rem Define path to project file
 set "PRJFILE=%~dp0\CSharp_D3D.sln"
 
-set "VisualStudioExpressName=VCExpress"
-
-if /I "%VCVER%" == "vc10" (
-  set "DevEnvDir=%VS100COMNTOOLS%..\IDE"
-) else if /I "%VCVER%" == "vc11" (
-  set "DevEnvDir=%VS110COMNTOOLS%..\IDE"
-  rem Visual Studio Express starting from VS 2012 is called "for Windows Desktop"
-  rem and has a new name for executable - WDExpress
-  set "VisualStudioExpressName=WDExpress"
-) else if /I "%VCVER%" == "vc12" (
-  set "DevEnvDir=%VS120COMNTOOLS%..\IDE"
-  set "VisualStudioExpressName=WDExpress"
-) else if /I "%VCVER%" == "vc14" (
-  set "DevEnvDir=%VS140COMNTOOLS%..\IDE"
-  set "VisualStudioExpressName=WDExpress"
-) else (
-  echo Error: wrong VS identifier
-  exit /B
-)
-
 rem Launch Visual Studio - either professional (devenv) or Express, as available
 if exist "%DevEnvDir%\devenv.exe"  (
   start "%DevEnvDir%\devenv.exe" "%PRJFILE%"
--- samples/qt/FuncDemo/genproj.bat
+++ samples/qt/FuncDemo/genproj.bat
@@ -8,29 +8,6 @@ REM third argument specifies Debug or Release mode
 
 call "%~dp0env.bat" %1 %2 %3
 
-if not "%VCVER%" == "" (
-    if /I "%VCVER%" == "vc10" (
-        set VCVER=vc10
-        set "VCVARS=%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
-    ) else if /I "%VCVER%" == "vc11" (
-        set VCVER=vc11
-        set "VCVARS=%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
-    ) else if /I "%VCVER%" == "vc12" (
-        set VCVER=vc12
-        set "VCVARS=%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-    ) else if /I "%VCVER%" == "vc14" (
-        set VCVER=vc14
-        set "VCVARS=%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
-    ) else (
-        echo Error: first argument ^(%VCVER%^) should specify supported version of Visual C++,
-        echo one of: vc10 ^(VS 2010 SP3^), vc11 ^(VS 2012 SP3^), vc12 ^(VS 2013^) or vc14 ^(VS 2015^)
-        exit
-    )
-)
-
-if ["%ARCH%"] == ["32"] set VCARCH=x86
-if ["%ARCH%"] == ["64"] set VCARCH=amd64
-
 call "%VCVARS%" %VCARCH%
 
 qmake -tp vc -r -o FuncDemo.sln FuncDemo0.pro
