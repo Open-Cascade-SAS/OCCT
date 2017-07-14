63bd365eed29224225316c9093b5ec5905ef1d79
mkv <mkv@opencascade.com>
2017-07-14 19:56:05 +0300

    Test for 0023176: Intersection of cylinder and cone produces open curve

--- src/XDEDRAW/XDEDRAW_Views.hxx
+++ src/XDEDRAW/XDEDRAW_Views.hxx
@@ -2,15 +2,15 @@
 // Created by: Irina KRYLOVA
 // Copyright (c) 2016 OPEN CASCADE SAS
 //
-// This file is part  Open CASCADE Technology software library.
+// This file is part of Open CASCADE Technology software library.
 //
 // This library is free software; you can redistribute it and/or modify it under
-// the terms  the GNU Lesser General Public License version 2.1 as published
+// the terms of the GNU Lesser General Public License version 2.1 as published
 // by the Free Software Foundation, with special exception defined in the file
 // OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
-// distribution for complete text  the license and disclaimer  any warranty.
+// distribution for complete text of the license and disclaimer of any warranty.
 //
-// Alternatively, this file may be used under the terms  Open CASCADE
+// Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
 #ifndef _XDEDRAW_Views_HeaderFile
