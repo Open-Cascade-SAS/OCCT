7f83633f7efd083277e8ae0598157b20afd06706
abv <abv@opencascade.com>
2017-10-09 13:19:20 +0300

    0029196: Tests - some test cases fail with Intel HD graphics
    Test bugs vis bug26357 is revised to check colors in more robust way.
    In tests bugs vis bug29091_*, size of produced file is checked to be within range of known valid sizes, rather than hard-coded (per-platform) exact sizes.
    FPE signals are disabled for test bugs vis bug29127 to prevent exceptions cause by software OpenGl driver.
    
    File data/occ/Box.brep is removed to avoid confusion with file box.brep in public data files used in tests.
    
    Two additional non-alphabetic-order combinations of complex types are handled in RWStepAP214_ReadWriteModule.cxx

--- src/RWStepAP214/RWStepAP214_ReadWriteModule.cxx
+++ src/RWStepAP214/RWStepAP214_ReadWriteModule.cxx
@@ -3401,6 +3401,15 @@ Standard_Integer RWStepAP214_ReadWriteModule::CaseStep
                (types(7).IsEqual(StepType(271)))) {
         return 323;
       }
+      else if ((types(1).IsEqual(StepType(40))) &&
+               (types(2).IsEqual(StepType(41))) &&
+               (types(3).IsEqual(StepType(240))) &&
+               (types(4).IsEqual(StepType(49))) &&
+               (types(5).IsEqual(StepType(247))) &&
+               (types(6).IsEqual(StepType(144))) &&
+               (types(7).IsEqual(StepType(271)))) {
+        return 323;
+      }
     }
     // Added by FMA
     else if (NbComp == 6) {
@@ -3629,6 +3638,11 @@ Standard_Integer RWStepAP214_ReadWriteModule::CaseStep
                (types(2).IsEqual(StepType(264))) &&
                (types(3).IsEqual(StepType(336)))) {
         return 335;
+      }
+      else if ((types(1).IsEqual(StepType(169))) &&
+               (types(2).IsEqual(StepType(336))) &&
+               (types(3).IsEqual(StepType(264)))) {
+	    return 335;
       }
       else if ((types(1).IsEqual(StepType(143))) &&
                (types(2).IsEqual(StepType(188))) &&
