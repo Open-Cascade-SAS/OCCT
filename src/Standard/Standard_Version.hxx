94c00556ea33f3895196b30c45b1fa901ad4c377
bugmaster <bugmaster@opencascade.com>
2021-02-02 11:51:56 +0300

    Update version up to 7.5.1

--- src/Standard/Standard_Version.hxx
+++ src/Standard/Standard_Version.hxx
@@ -42,7 +42,7 @@
 //! - "dev" for development version between releases
 //! - "beta..." or "rc..." for beta releases or release candidates
 //! - "project..." for version containing project-specific fixes
-#define OCC_VERSION_DEVELOPMENT   "dev"
+//#define OCC_VERSION_DEVELOPMENT   "dev"
 
 // Derived (manually): version as real and string (major.minor)
 #define OCC_VERSION               7.5
