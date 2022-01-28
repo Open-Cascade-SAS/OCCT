2b5fcf89b270ffcf37875f916d3ab38436b78f5f
aml <aml@opencascade.com>
2022-01-28 18:39:49 +0300

    0032810: Coding - missing Standard_EXPORT in Standard_ErrorHandler

--- src/Standard/Standard_ErrorHandler.hxx
+++ src/Standard/Standard_ErrorHandler.hxx
@@ -155,13 +155,22 @@ public:
     DEFINE_STANDARD_ALLOC
 
     //! Registers this callback object in the current error handler (if found).
-    void RegisterCallback();
+    #if defined(OCC_CONVERT_SIGNALS)
+      Standard_EXPORT
+    #endif
+      void RegisterCallback();
 
     //! Unregisters this callback object from the error handler.
-    void UnregisterCallback();
+    #if defined(OCC_CONVERT_SIGNALS)
+      Standard_EXPORT
+    #endif
+      void UnregisterCallback();
 
     //! Destructor
-    virtual ~Callback();
+    #if defined(OCC_CONVERT_SIGNALS)
+      Standard_EXPORT
+    #endif
+      virtual ~Callback();
 
     //! The callback function to perform necessary callback action.
     //! Called by the exception handler when it is being destroyed but
@@ -171,7 +180,10 @@ public:
   protected:
 
     //! Empty constructor
-    Callback();
+    #if defined(OCC_CONVERT_SIGNALS)
+      Standard_EXPORT
+    #endif
+      Callback();
 
   private:
     Standard_Address myHandler;
