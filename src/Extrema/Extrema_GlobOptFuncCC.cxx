f0c6e6fc8d1862414826b07cb485d950a45e7efb
akaftasev <akaftasev@opencascade.com>
2023-10-25 12:32:49 +0100

    0033515: Modeling Algorithm - BRepExtrema_DistShapeShape poduces wrong result
    Changed redundant square operation

--- src/Extrema/Extrema_GlobOptFuncCC.cxx
+++ src/Extrema/Extrema_GlobOptFuncCC.cxx
@@ -43,7 +43,7 @@ static Standard_Boolean _Value(const Adaptor3d_Curve& C1,
     return Standard_False;
   }
 
-  F = C2.Value(v).SquareDistance(C1.Value(u));
+  F = C2.Value(v).Distance(C1.Value(u));
   return Standard_True;
 }
 
@@ -64,7 +64,7 @@ static Standard_Boolean _Value(const Adaptor2d_Curve2d& C1,
     return Standard_False;
   }
 
-  F = C2.Value(v).SquareDistance(C1.Value(u));
+  F = C2.Value(v).Distance(C1.Value(u));
   return Standard_True;
 }
 
