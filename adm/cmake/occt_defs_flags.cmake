0a73cac7597be49dd8ff14e18f7072e731b03097
jfa <jfa@opencascade.com>
2024-02-09 12:29:15 +0000

    Configuration - CMake symbol isolation issue on Windows #373
    Fixed Salome compilation problem on Windows by M.Bernhard

--- adm/cmake/occt_defs_flags.cmake
+++ adm/cmake/occt_defs_flags.cmake
@@ -26,7 +26,7 @@ endif()
 
 if (MSVC)
   # suppress C26812 on VS2019/C++20 (prefer 'enum class' over 'enum')
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:precise /wd\"26812\"")
+  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:precise /wd26812")
   # suppress warning on using portable non-secure functions in favor of non-portable secure ones
   add_definitions (-D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE)
 else()
