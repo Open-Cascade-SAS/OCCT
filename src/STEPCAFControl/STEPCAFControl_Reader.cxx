c1620d29fa1ba726ad8cd54f19d3b9e72e426217
ichesnok <ichesnok@opencascade.com>
2024-03-08 13:38:18 +0000

    0033631: Data Exchange, Step import - Crash by reading STEP file
    Creating Poly_Triangulation from ComplexTriangulatedSurfaceSet was fixed.

--- src/STEPCAFControl/STEPCAFControl_Reader.cxx
+++ src/STEPCAFControl/STEPCAFControl_Reader.cxx
@@ -1996,7 +1996,7 @@ Handle(Poly_Triangulation) createMesh(const Handle(StepVisual_ComplexTriangulate
   const Standard_Integer aNbNormals = theTriangulatedSufaceSet->NbNormals();
   // Number of pairs (Point, Normal). It is possible for one point to have multiple normals. This is
   // useful when the underlying surface is not C1 continuous.
-  const Standard_Integer aNbPairs = aNbNormals > 1 ? theTriangulatedSufaceSet->NbPnindex() : aNodes->Length();
+  const Standard_Integer aNbPairs = aNbNormals > 1 ? theTriangulatedSufaceSet->Pnmax() : aNodes->Length();
   const Standard_Boolean aHasNormals = aNbNormals > 0;
 
   // Counting number of triangles in the triangle strips list.
@@ -2021,7 +2021,7 @@ Handle(Poly_Triangulation) createMesh(const Handle(StepVisual_ComplexTriangulate
 
   for (Standard_Integer j = 1; j <= aNbPairs; ++j)
   {
-    const gp_XYZ& aPoint = aNodes->Value(aNbNormals > 1 ? theTriangulatedSufaceSet->PnindexValue(j) : j);
+    const gp_XYZ& aPoint = aNodes->Value((aNbNormals > 1 && theTriangulatedSufaceSet->NbPnindex() > 0) ? theTriangulatedSufaceSet->PnindexValue(j) : j);
     aMesh->SetNode(j, theFact * aPoint);
   }
 
