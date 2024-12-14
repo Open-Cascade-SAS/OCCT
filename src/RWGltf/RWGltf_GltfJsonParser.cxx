Open-Cascade-SAS/OCCT

commit bbbb8ea79dca3fe931eda9b3e96d8991b5a1b1f5

GLTF Import - Metadata support #184
Limited support of metadata import is added to RWGltf_GltfJsonParser.
Following Json data types are currently supported: int32, double, string,
array of int32, array of double, array of strings, Json object.
Notable unsupported types are: binary data, array of Json objects.
Metadata is processed for nodes and meshes.
Tests "gltf_export" are updated with import testing and renamed to "gltf".

Method RWGltf_GltfJsonParser::gltfParseSceneNode() is slightly
refactored: parsing of transformations and transformation matrices is
moved into separate functions.

src/RWGltf/RWGltf_GltfJsonParser.hxx
@@ -297,17 +297,19 @@ protected:
   void bindNodeShape (TopoDS_Shape& theShape,
                       const TopLoc_Location& theLoc,
                       const TCollection_AsciiString& theNodeId,
-                      const RWGltf_JsonValue* theUserName)
+                      const RWGltf_JsonValue* theUserName,
+                      const Handle(TDataStd_NamedData)& theExtras)
   {
-    bindNamedShape (theShape, ShapeMapGroup_Nodes, theLoc, theNodeId, theUserName);
+    bindNamedShape (theShape, ShapeMapGroup_Nodes, theLoc, theNodeId, theUserName, theExtras);
   }
 
   //! Bind name attribute.
   void bindMeshShape (TopoDS_Shape& theShape,
                       const TCollection_AsciiString& theMeshId,
-                      const RWGltf_JsonValue* theUserName)
+                      const RWGltf_JsonValue* theUserName,
+                      const Handle(TDataStd_NamedData)& theExtras)
   {
-    bindNamedShape (theShape, ShapeMapGroup_Meshes, TopLoc_Location(), theMeshId, theUserName);
+    bindNamedShape (theShape, ShapeMapGroup_Meshes, TopLoc_Location(), theMeshId, theUserName, theExtras);
   }
 
   //! Find named shape.
@@ -329,7 +331,8 @@ protected:
                                        ShapeMapGroup theGroup,
                                        const TopLoc_Location& theLoc,
                                        const TCollection_AsciiString& theId,
-                                       const RWGltf_JsonValue* theUserName);
+                                       const RWGltf_JsonValue* theUserName,
+                                       const Handle(TDataStd_NamedData)& theExtras);
 
   //! Find named shape.
   bool findNamedShape (TopoDS_Shape& theShape,
@@ -405,12 +408,38 @@ protected:
   };
 #endif
 protected:
-
   //! Print message about invalid glTF syntax.
-  void reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg, Message_Gravity theGravity);
+  void reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg, Message_Gravity theGravity) const;
+
+private:
+  //! Parse transformation matrix of the node.
+  //! @param theSceneNodeId Name of the node. Used only for printing messages.
+  //! @param theMatrixVal Json value containing transformation matrix.
+  //! @param theResult TopLoc_Location object where result of parsing will be written.
+  //! @param If true - parsing was successful, transformation is written into @p theResult.
+  //!        If true - failed to parse, @p theResult is unchanged.
+  bool parseTransformationMatrix(const TCollection_AsciiString& theSceneNodeId,
+                                 const RWGltf_JsonValue& theMatrixVal,
+                                 TopLoc_Location& theResult) const;
+
+  //! Parse transformation components of the node.
+  //! @param theSceneNodeId Name of the node. Used only for printing messages.
+  //! @param theRotationVal Json value containing rotation component of transformation.
+  //!        May be null in which case it is ignored.
+  //! @param theScaleVal Json value containing scale component of transformation.
+  //!        May be null in which case it is ignored.
+  //! @param theTranslationVal Json value containing translation component of transformation.
+  //!        May be null in which case it is ignored.
+  //! @param theResult TopLoc_Location object where result of parsing will be written.
+  //! @param If true - parsing was successful, transformation is written into @p theResult.
+  //!        If true - failed to parse, @p theResult is unchanged.
+  bool parseTransformationComponents(const TCollection_AsciiString& theSceneNodeId,
+                                     const RWGltf_JsonValue* theRotationVal,
+                                     const RWGltf_JsonValue* theScaleVal,
+                                     const RWGltf_JsonValue* theTranslationVal,
+                                     TopLoc_Location& theResult) const;
 
 protected:
-
   TopTools_SequenceOfShape*        myRootShapes;    //!< sequence of result root shapes
   RWMesh_NodeAttributeMap*         myAttribMap;     //!< shape attributes
   NCollection_IndexedMap<TCollection_AsciiString>*

tests/metadata/gltf/A1
@@ -1,0 +1,29 @@
+# !!!! This file is generated automatically, do not edit manually! See end script
+set filename bug28345_30338.stp
+set ref_size 5896
+set check_metadata 1
+set ref_metadata {Property for [0:1:1:1]:
+H_CIP : 55.545955351400004
+Property for [0:1:1:1:1]:
+H : 45
+E : 55
+B : 16
+I : 15
+A : 3
+D : 3
+C : 140
+F : 0.29999999999999999
+DESCRIPTION : 
+MODELED_BY : 
+Property for [0:1:1:1:2]:
+H : 45
+E : 55
+B : 16
+I : 15
+A : 3
+D : 3
+C : 140
+F : 0.29999999999999999
+DESCRIPTION : 
+MODELED_BY : 
+}

tests/metadata/gltf/A2
@@ -1,0 +1,26 @@
+# !!!! This file is generated automatically, do not edit manually! See end script
+set filename bug28389_CONFIDENTIAL_SHEET_METAL_F3D.stp
+set ref_size 86278
+set check_metadata 1
+set ref_metadata {Property for [0:1:1:1]:
+yCenterOfGravity : 0.1148447698
+Ixz : 9.3210000000000004e-07
+Izx : 9.3210000000000004e-07
+OriginY : 0
+Surface : 0.34595390710000001
+Volume : 0.0001375456
+Iyz : -1.2030000000000001e-07
+zCenterOfGravity : -0.056064514900000001
+Ixy : 2.044e-07
+Iyy : 3.6385e-06
+xCenterOfGravity : -0.12673526900000001
+Density : 1000
+Izz : 3.3558999999999999e-06
+Ixx : 1.7740000000000001e-06
+Izy : -1.2030000000000001e-07
+Mass : 0.13754561600000001
+Iyx : 2.044e-07
+OriginX : 0
+OriginZ : 0
+JoggleFormula : 
+}

tests/metadata/gltf/A3
@@ -1,0 +1,23 @@
+# !!!! This file is generated automatically, do not edit manually! See end script
+set filename bug28444_nist_ftc_06_asme1_ct5240_rd.stp
+set ref_size 85383
+set check_metadata 1
+set ref_metadata {Property for [0:1:1:1]:
+yCenterOfGravity : 0.0289950044
+Ixz : 0
+Izx : 0
+Surface : 0.28317040780000002
+Volume : 0.0033238733999999999
+Iyz : -1.3068999999999999e-06
+zCenterOfGravity : -0.10963042420000001
+Ixy : 0
+Iyy : 4.46342e-05
+xCenterOfGravity : -0
+Density : 1000
+Izz : 2.63853e-05
+Length : 0
+Ixx : 2.16819e-05
+Izy : -1.3068999999999999e-06
+Mass : 3.3238733752999998
+Iyx : 0
+}

tests/metadata/gltf/A4
@@ -1,3 +1,4 @@
 # !!!! This file is generated automatically, do not edit manually! See end script
 set filename bug29525_rev_part_neu_01.prt_converted_from_datakit.stp
 set ref_size 80996
+set check_metadata 0

tests/metadata/gltf/A5
@@ -1,0 +1,18 @@
+# !!!! This file is generated automatically, do not edit manually! See end script
+set filename bug29633_nist_ctc_05_asme1_ap242-1.stp
+set ref_size 69902
+set check_metadata 1
+set ref_metadata {Property for [0:1:1:1]:
+FILESIZE : 1495040
+GDT_STANDARD : 5302
+MaterialMultipleAssigned : FALSE
+ATTR_VERSION : 18.3.001
+FILESAVETIME : Tue Dec 09 03:47:24 2014
+Part Number : NIST PMI CTC 05 ASME1
+Revision : D
+CAD_SOURCE : ug
+MTIME : 1418096844
+MaterialMissingAssignments : TRUE
+FILENAME : nist_ctc_05_asme1.prt
+Description : NIST PMI test model downloaded from http://go.usa.gov/mGVm
+}

tests/metadata/gltf/A6
@@ -1,0 +1,41 @@
+# !!!! This file is generated automatically, do not edit manually! See end script
+set filename bug29803.stp
+set ref_size 17032
+set check_metadata 1
+set ref_metadata {Property for [0:1:1:1]:
+OUT_MASS : 50.813477444850157
+RELIEF_DIA : 21.005800000000001
+HELIX_LENGTH : 0
+OUT_OAL : 78.049999999992593
+HELIX_START : 0
+OUT_SHANK_LEN : 27.2499999999926
+RCA_SIZE : 0
+SHANK_UNDER : 0
+OUT_REF_LEN : 50.799999999999997
+BODY_LENGTH : 48.514000000000003
+THEO_BLADE_DIA : 11.074400000000001
+BODY_DIA : 0
+DRILL_DEPTH : 47.625
+SHANK_SIZE : 16
+FLUTE_LENGTH : 42.468800000000002
+OUT_SHANK_DIA : 15.999999999999501
+PRIORITY : 0
+OUT_DRILL_DEPTH : 44.754800000000003
+SCREW_HOLE_SKEW : 1.1000000000000001
+SHANK_DIAMETER : 15.999999999999501
+DESCRIPTION : T-A HOLDER
+SS_FLANGE : NO
+MODELED_BY : LSD
+STANDARD_BODY_DIA : Y
+DEEP_HOLE_WEBSITE : 
+ITEM_NUM : HOLDER
+LENGTH : STUB
+FINISH : BLACK OXIDE
+NOTES : 
+SHANK_IM : M
+FLUTE : STRAIGHT
+SHANK : ER
+MATERIAL : STEEL
+SERIES : Y
+DEEP_HOLE_NOTES : 
+}

commit 25906563a3b384dbbd4bbadc1b88e84abe8df7f0

Coding, GLTF - Isolate RapidJSon part #192
Regression after #184 where some methods are not isolated.

src/RWGltf/RWGltf_GltfJsonParser.cxx
@@ -35,6 +35,10 @@
 #ifdef HAVE_RAPIDJSON
 namespace
 {
+// Auxiliary macros for formatting message.
+#define reportGltfError(theMsg)   reportGltfSyntaxProblem(TCollection_AsciiString() + theMsg, Message_Fail);
+#define reportGltfWarning(theMsg) reportGltfSyntaxProblem(TCollection_AsciiString() + theMsg, Message_Warning);
+
   //! Material extension.
   static const char THE_KHR_materials_common[] = "KHR_materials_common";
   static const char THE_KHR_binary_glTF[]      = "KHR_binary_glTF";
@@ -416,21 +420,6 @@ void RWGltf_GltfJsonParser::GltfElementMap::Init (const TCollection_AsciiString&
     }
   }
 }
-#endif
-
-// Auxiliary macros for formatting message.
-#define reportGltfError(theMsg)   reportGltfSyntaxProblem(TCollection_AsciiString() + theMsg, Message_Fail);
-#define reportGltfWarning(theMsg) reportGltfSyntaxProblem(TCollection_AsciiString() + theMsg, Message_Warning);
-
-// =======================================================================
-// function : reportGltfSyntaxProblem
-// purpose  :
-// =======================================================================
-void RWGltf_GltfJsonParser::reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg,
-                                                    Message_Gravity theGravity) const
-{
-  Message::Send (myErrorPrefix + theMsg, theGravity);
-}
 
 // =======================================================================
 // function : parseTransformationMatrix
@@ -594,6 +583,17 @@ bool RWGltf_GltfJsonParser::parseTransformationComponents(const TCollection_Asci
     theResult = TopLoc_Location(aTrsf);
   }
   return true;
+}
+#endif
+
+// =======================================================================
+// function : reportGltfSyntaxProblem
+// purpose  :
+// =======================================================================
+void RWGltf_GltfJsonParser::reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg,
+                                                    Message_Gravity theGravity) const
+{
+  Message::Send (myErrorPrefix + theMsg, theGravity);
 }
 
 // =======================================================================

src/RWGltf/RWGltf_GltfJsonParser.hxx
@@ -406,10 +406,6 @@ protected:
     const RWGltf_JsonValue* myRoot;
 
   };
-#endif
-protected:
-  //! Print message about invalid glTF syntax.
-  void reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg, Message_Gravity theGravity) const;
 
 private:
   //! Parse transformation matrix of the node.
@@ -438,6 +434,10 @@ private:
                                      const RWGltf_JsonValue* theScaleVal,
                                      const RWGltf_JsonValue* theTranslationVal,
                                      TopLoc_Location& theResult) const;
+#endif
+protected:
+  //! Print message about invalid glTF syntax.
+  void reportGltfSyntaxProblem (const TCollection_AsciiString& theMsg, Message_Gravity theGravity) const;
 
 protected:
   TopTools_SequenceOfShape*        myRootShapes;    //!< sequence of result root shapes