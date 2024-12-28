a90cf73afdc59c4abe83603e93a6ce2efcb912f6
dpasukhi <dpasukhi@opencascade.com>
2024-12-28 18:56:47 +0000

    Coding - Coding - Refactor TopClass to remove gxx files #218
    Refactor TopClass and BRepClass3d files by renaming and removing obsolete Classifier3d implementations

--- src/BRepClass3d/BRepClass3d_SolidPassiveClassifier.hxx
+++ src/BRepClass3d/BRepClass3d_SolidPassiveClassifier.hxx
@@ -1,7 +1,5 @@
-// Created on: 1994-04-18
-// Created by: Laurent BUCHARD
 // Copyright (c) 1994-1999 Matra Datavision
-// Copyright (c) 1999-2014 OPEN CASCADE SAS
+// Copyright (c) 1999-2024 OPEN CASCADE SAS
 //
 // This file is part of Open CASCADE Technology software library.
 //
@@ -17,94 +15,61 @@
 #ifndef _BRepClass3d_SolidPassiveClassifier_HeaderFile
 #define _BRepClass3d_SolidPassiveClassifier_HeaderFile
 
+#include <BRepClass3d_Intersector3d.hxx>
 #include <Standard.hxx>
+#include <Standard_Boolean.hxx>
 #include <Standard_DefineAlloc.hxx>
 #include <Standard_Handle.hxx>
-
-#include <Standard_Boolean.hxx>
+#include <TopAbs_Orientation.hxx>
+#include <TopAbs_State.hxx>
 #include <TopoDS_Face.hxx>
 #include <gp_Lin.hxx>
-#include <TopAbs_State.hxx>
-#include <BRepClass3d_Intersector3d.hxx>
-#include <TopAbs_Orientation.hxx>
+
 class Standard_DomainError;
 class BRepClass3d_Intersector3d;
 class gp_Lin;
 class TopoDS_Face;
 
-
-
-class BRepClass3d_SolidPassiveClassifier 
+class BRepClass3d_SolidPassiveClassifier
 {
 public:
-
   DEFINE_STANDARD_ALLOC
 
-  
   //! Creates an undefined classifier.
   Standard_EXPORT BRepClass3d_SolidPassiveClassifier();
-  
+
   //! Starts  a  classification process.   The  point to
   //! classify is the origin of  the  line <L>.  <P>  is
   //! the original length of the segment on <L>  used to
   //! compute  intersections.   <Tol> is the   tolerance
   //! attached to the intersections.
-  Standard_EXPORT void Reset (const gp_Lin& L, const Standard_Real P, const Standard_Real Tol);
-  
+  Standard_EXPORT void Reset(const gp_Lin& L, const Standard_Real P, const Standard_Real Tol);
+
   //! Updates  the classification process with  the face
   //! <F> from the boundary.
-  Standard_EXPORT void Compare (const TopoDS_Face& F, const TopAbs_Orientation Or);
-  
+  Standard_EXPORT void Compare(const TopoDS_Face& F, const TopAbs_Orientation Or);
+
   //! Returns the current value of the parameter.
-    Standard_Real Parameter() const;
-  
+  Standard_Real Parameter() const { return myParam; }
+
   //! Returns True if an intersection is computed.
-  Standard_Boolean HasIntersection() const;
-  
+  Standard_Boolean HasIntersection() const { return hasIntersect; }
+
   //! Returns the intersecting algorithm.
-    BRepClass3d_Intersector3d& Intersector();
-  
+  BRepClass3d_Intersector3d& Intersector() { return myIntersector; }
+
   //! Returns the current state of the point.
-    TopAbs_State State() const;
-
-
-
-
-protected:
-
-
-
-
+  TopAbs_State State() const { return myState; }
 
 private:
-
-
-
-  Standard_Boolean isSet;
-  TopoDS_Face myFace;
-  gp_Lin myLin;
-  Standard_Real myParam;
-  Standard_Real myTolerance;
-  TopAbs_State myState;
-  Standard_Boolean hasIntersect;
+  Standard_Boolean          isSet;
+  TopoDS_Face               myFace;
+  gp_Lin                    myLin;
+  Standard_Real             myParam;
+  Standard_Real             myTolerance;
+  TopAbs_State              myState;
+  Standard_Boolean          hasIntersect;
   BRepClass3d_Intersector3d myIntersector;
-
-
 };
-
-#define TheIntersector BRepClass3d_Intersector3d
-#define TheIntersector_hxx <BRepClass3d_Intersector3d.hxx>
-#define TopClass_Classifier3d BRepClass3d_SolidPassiveClassifier
-#define TopClass_Classifier3d_hxx <BRepClass3d_SolidPassiveClassifier.hxx>
-
-#include <TopClass_Classifier3d.lxx>
-
-#undef TheIntersector
-#undef TheIntersector_hxx
-#undef TopClass_Classifier3d
-#undef TopClass_Classifier3d_hxx
-
-
-
 
 #endif // _BRepClass3d_SolidPassiveClassifier_HeaderFile
