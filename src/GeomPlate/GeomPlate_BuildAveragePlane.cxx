16ed3b4cede2adb8b4ac14525e303771d4d50033
dpasukhi <dpasukhi@opencascade.com>
2025-01-28 12:11:53 +0000

    Modeling - Resetting Plane YVector #296
    Fix calculation of myOY in GeomPlate_BuildAveragePlane to use cross product with OZ

--- src/GeomPlate/GeomPlate_BuildAveragePlane.cxx
+++ src/GeomPlate/GeomPlate_BuildAveragePlane.cxx
@@ -439,7 +439,7 @@ void GeomPlate_BuildAveragePlane::BasePlan(const gp_Vec& OZ)
       || ((Abs(n1) <= myTol) && (Abs(n3) <= myTol)))
   {
     myOX.SetCoord(V3(1), V3(2), V3(3));
-    myOY.SetCoord(0, 0, 0);
+    myOY = OZ ^ myOX;
   }
   else
   {
