239cadd52e1a1114899800a368ea914d6a327028
dpasukhi <dpasukhi@opencascade.com>
2025-01-30 23:54:56 +0000

    Enhance version handling in CMake to support development version detection and include Git hash in version string

--- adm/cmake/version.cmake
+++ adm/cmake/version.cmake
@@ -6,15 +6,14 @@
 #  OCC_VERSION_MINOR       : (integer) number identifying minor version
 #  OCC_VERSION_MAINTENANCE : (integer) number identifying maintenance version
 #  OCC_VERSION_DEVELOPMENT : (string)  if defined, indicates development or modified version
-#                              in case of release, comment the line with setting this variable
+#                              in case of release, remove the value
 #
 #  Sample values of OCC_VERSION_DEVELOPMENT:
 #  - "dev" for development version between releases
 #  - "beta..." or "rc..." for beta releases or release candidates
 #  - "project..." for version containing project-specific fixes
 #
-#  CMake variable USE_GIT_REVISION (bool) will be used to extend the result
-#    version string with Git revision number in case of development version is not empty.
+#  For development version git commit hash can be added to the version string
 #======================================================================
 
 set (OCC_VERSION_MAJOR 7 )
