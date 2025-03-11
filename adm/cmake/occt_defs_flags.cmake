092f0a7230e5b36830f2068d83983260f769d212
dpasukhi <dpasukhi@opencascade.com>
2025-03-11 09:05:35 +0000

    Configuration - Comment out OCC_CONVERT_SIGNALS definition in CMake

--- adm/cmake/occt_defs_flags.cmake
+++ adm/cmake/occt_defs_flags.cmake
@@ -42,7 +42,7 @@ else()
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations")
     set (CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -Wno-deprecated-declarations")
   endif()
-  add_definitions(-DOCC_CONVERT_SIGNALS)
+  #add_definitions(-DOCC_CONVERT_SIGNALS)
 endif()
 
 # enable structured exceptions for MSVC
