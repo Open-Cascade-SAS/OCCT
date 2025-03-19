65039a4a97cf77fba6676019561e1a4db2dea069
dpasukhi <dpasukhi@opencascade.com>
2025-03-19 14:32:54 +0000

    Refactor - Update package path for TKOpenGlesTest and remove obsolete OpenGlTest files

--- src/Draw/TKOpenGlesTest/OpenGlTest/FILES.cmake
+++
@@ -1,8 +0,0 @@
-# Source files for OpenGlTest package
-set(OCCT_OpenGlTest_FILES_LOCATION "${CMAKE_CURRENT_LIST_DIR}")
-
-set(OCCT_OpenGlTest_FILES
-  OpenGlTest.cxx
-  OpenGlTest.hxx
-  OpenGlTest_Commands.cxx
-)
--- src/Draw/TKOpenGlesTest/PACKAGES.cmake
+++ src/Draw/TKOpenGlesTest/PACKAGES.cmake
@@ -1,4 +1,4 @@
 # Auto-generated list of packages for TKOpenGlesTest toolkit
 set(OCCT_TKOpenGlesTest_LIST_OF_PACKAGES
-  OpenGlTest
+  ../TKOpenGlTest/OpenGlTest
 )
