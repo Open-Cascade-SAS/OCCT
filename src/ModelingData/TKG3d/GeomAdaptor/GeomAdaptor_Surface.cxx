90d847a2cb67b0046d3737ee512ace4b0b419916
dpasukhi <dpasukhi@opencascade.com>
2025-05-25 09:05:27 +0100

    Modeling - BSpline Surf caching mechanism perf research
    Refactor D0 method to improve cache handling in GeomAdaptor_Surface

--- src/ModelingData/TKG3d/GeomAdaptor/GeomAdaptor_Surface.cxx
+++ src/ModelingData/TKG3d/GeomAdaptor/GeomAdaptor_Surface.cxx
@@ -739,8 +739,13 @@ void GeomAdaptor_Surface::D0(const Standard_Real U, const Standard_Real V, gp_Pn
     case GeomAbs_BezierSurface:
     case GeomAbs_BSplineSurface:
       if (mySurfaceCache.IsNull() || !mySurfaceCache->IsCacheValid(U, V))
-        RebuildCache(U, V);
-      mySurfaceCache->D0(U, V, P);
+      {
+        mySurface->D0(U, V, P);
+      }
+      else
+      {
+        mySurfaceCache->D0(U, V, P);
+      }
       break;
 
     case GeomAbs_OffsetSurface:
