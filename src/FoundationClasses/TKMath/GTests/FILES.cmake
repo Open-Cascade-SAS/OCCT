0d30c935e36a09ee0f3eb2293df52f431dab5b60
dpasukhi <dpasukhi@opencascade.com>
2025-08-07 10:06:22 +0100

    Modeling, Bnd_Box - CornerMax incorrect reasliation
    Fixed CornerMax with getting valid values from Bnd_Box.
    Before we are extracting Min.
    Added extended GTest tests.

--- src/FoundationClasses/TKMath/GTests/FILES.cmake
+++ src/FoundationClasses/TKMath/GTests/FILES.cmake
@@ -3,6 +3,7 @@ set(OCCT_TKMath_GTests_FILES_LOCATION "${CMAKE_CURRENT_LIST_DIR}")
 
 set(OCCT_TKMath_GTests_FILES
   Bnd_BoundSortBox_Test.cxx
+  Bnd_Box_Test.cxx
   ElCLib_Test.cxx
   math_DoubleTab_Test.cxx
   math_Matrix_Test.cxx
