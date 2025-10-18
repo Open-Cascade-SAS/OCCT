5e751a00b20a48a90266094eb9e407882b33d613
dpasukhi <dpasukhi@opencascade.com>
2025-10-18 17:50:56 +0100

    TCollection: correct FILES.cmake to include HAsciiString lxx
    Replace TCollection_AsciiString.lxx with TCollection_HAsciiString.lxx in the package file list so the HAsciiString implementation file is properly referenced.

--- src/FoundationClasses/TKernel/TCollection/FILES.cmake
+++ src/FoundationClasses/TKernel/TCollection/FILES.cmake
@@ -5,11 +5,11 @@ set(OCCT_TCollection_FILES
   TCollection.hxx
   TCollection_AsciiString.cxx
   TCollection_AsciiString.hxx
-  TCollection_AsciiString.lxx
   TCollection_ExtendedString.cxx
   TCollection_ExtendedString.hxx
   TCollection_HAsciiString.cxx
   TCollection_HAsciiString.hxx
+  TCollection_HAsciiString.lxx
   TCollection_HExtendedString.cxx
   TCollection_HExtendedString.hxx
 )
