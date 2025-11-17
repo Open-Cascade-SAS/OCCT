4bc1e63f9c64311b60a0a2478bf3122cf497d298
dkulikov <dkulikov@opencascade.com>
2025-11-17 09:33:14 +0000

    Fix format

--- src/FoundationClasses/TKMath/ElCLib/ElCLib.cxx
+++ src/FoundationClasses/TKMath/ElCLib/ElCLib.cxx
@@ -197,11 +197,11 @@ gp_Pnt ElCLib::HyperbolaValue(const Standard_Real U,
                               const Standard_Real MajorRadius,
                               const Standard_Real MinorRadius)
 {
-  const gp_XYZ& XDir = Pos.XDirection().XYZ();
-  const gp_XYZ& YDir = Pos.YDirection().XYZ();
-  const gp_XYZ& PLoc = Pos.Location().XYZ();
-  const Standard_Real A1 = MajorRadius * std::cosh(U);
-  const Standard_Real A2 = MinorRadius * std::sinh(U);
+  const gp_XYZ&       XDir = Pos.XDirection().XYZ();
+  const gp_XYZ&       YDir = Pos.YDirection().XYZ();
+  const gp_XYZ&       PLoc = Pos.Location().XYZ();
+  const Standard_Real A1   = MajorRadius * std::cosh(U);
+  const Standard_Real A2   = MinorRadius * std::sinh(U);
   return gp_Pnt(A1 * XDir.X() + A2 * YDir.X() + PLoc.X(),
                 A1 * XDir.Y() + A2 * YDir.Y() + PLoc.Y(),
                 A1 * XDir.Z() + A2 * YDir.Z() + PLoc.Z());
