b3c4a800127fb7a6edb13cb96e9152af07ac4694
dpasukhi <dpasukhi@opencascade.com>
2026-01-22 23:21:25 +0000

    Add tests for TopoDS_Builder with various bucket sizes

--- src/ModelingData/TKBRep/GTests/TopoDS_Builder_Test.cxx
+++ src/ModelingData/TKBRep/GTests/TopoDS_Builder_Test.cxx
@@ -488,3 +488,325 @@ TEST(TopoDS_Builder_Test, ZeroBucketSize_UsesDefault)
   aBuilder.Add(aCompound, BRepBuilderAPI_MakeVertex(gp_Pnt(0, 0, 0)));
   EXPECT_EQ(aCompound.TShape()->NbChildren(), 1);
 }
+
+//=================================================================================================
+// Test MakeWire with various bucket sizes
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MakeWire_VariousBucketSizes)
+{
+  TopoDS_Builder aBuilder;
+
+  // Small bucket (1)
+  {
+    TopoDS_Wire aWire;
+    aBuilder.MakeWire(aWire, 1);
+    EXPECT_FALSE(aWire.IsNull());
+    EXPECT_EQ(aWire.ShapeType(), TopAbs_WIRE);
+
+    // Add more edges than bucket size to test growth
+    for (int i = 0; i < 10; ++i)
+    {
+      aBuilder.Add(aWire, BRepBuilderAPI_MakeEdge(gp_Pnt(i, 0, 0), gp_Pnt(i + 1, 0, 0)));
+    }
+    EXPECT_EQ(aWire.TShape()->NbChildren(), 10);
+  }
+
+  // Large bucket (1000)
+  {
+    TopoDS_Wire aWire;
+    aBuilder.MakeWire(aWire, 1000);
+    EXPECT_FALSE(aWire.IsNull());
+
+    // Add a few edges
+    for (int i = 0; i < 5; ++i)
+    {
+      aBuilder.Add(aWire, BRepBuilderAPI_MakeEdge(gp_Pnt(i, 0, 0), gp_Pnt(i + 1, 0, 0)));
+    }
+    EXPECT_EQ(aWire.TShape()->NbChildren(), 5);
+  }
+}
+
+//=================================================================================================
+// Test MakeShell with various bucket sizes
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MakeShell_VariousBucketSizes)
+{
+  TopoDS_Builder aBuilder;
+
+  // Small bucket
+  {
+    TopoDS_Shell aShell;
+    aBuilder.MakeShell(aShell, 2);
+    EXPECT_FALSE(aShell.IsNull());
+    EXPECT_EQ(aShell.ShapeType(), TopAbs_SHELL);
+  }
+
+  // Medium bucket
+  {
+    TopoDS_Shell aShell;
+    aBuilder.MakeShell(aShell, 20);
+    EXPECT_FALSE(aShell.IsNull());
+  }
+
+  // Large bucket
+  {
+    TopoDS_Shell aShell;
+    aBuilder.MakeShell(aShell, 200);
+    EXPECT_FALSE(aShell.IsNull());
+  }
+}
+
+//=================================================================================================
+// Test MakeSolid with various bucket sizes
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MakeSolid_VariousBucketSizes)
+{
+  TopoDS_Builder aBuilder;
+
+  // Typical solid has 1-2 shells
+  {
+    TopoDS_Solid aSolid;
+    aBuilder.MakeSolid(aSolid, 1);
+    EXPECT_FALSE(aSolid.IsNull());
+    EXPECT_EQ(aSolid.ShapeType(), TopAbs_SOLID);
+  }
+
+  // Solid with potential voids
+  {
+    TopoDS_Solid aSolid;
+    aBuilder.MakeSolid(aSolid, 5);
+    EXPECT_FALSE(aSolid.IsNull());
+  }
+}
+
+//=================================================================================================
+// Test MakeCompSolid with various bucket sizes
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MakeCompSolid_VariousBucketSizes)
+{
+  TopoDS_Builder aBuilder;
+
+  // Small
+  {
+    TopoDS_CompSolid aCS;
+    aBuilder.MakeCompSolid(aCS, 2);
+    EXPECT_FALSE(aCS.IsNull());
+    EXPECT_EQ(aCS.ShapeType(), TopAbs_COMPSOLID);
+  }
+
+  // Large
+  {
+    TopoDS_CompSolid aCS;
+    aBuilder.MakeCompSolid(aCS, 100);
+    EXPECT_FALSE(aCS.IsNull());
+  }
+}
+
+//=================================================================================================
+// Test MakeCompound with various bucket sizes and population
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MakeCompound_VariousBucketSizes)
+{
+  TopoDS_Builder aBuilder;
+
+  // Bucket size 1 - will need to grow
+  {
+    TopoDS_Compound aC;
+    aBuilder.MakeCompound(aC, 1);
+    for (int i = 0; i < 100; ++i)
+    {
+      aBuilder.Add(aC, BRepBuilderAPI_MakeVertex(gp_Pnt(i, 0, 0)));
+    }
+    EXPECT_EQ(aC.TShape()->NbChildren(), 100);
+  }
+
+  // Bucket size exactly matching expected children
+  {
+    TopoDS_Compound aC;
+    aBuilder.MakeCompound(aC, 50);
+    for (int i = 0; i < 50; ++i)
+    {
+      aBuilder.Add(aC, BRepBuilderAPI_MakeVertex(gp_Pnt(i, 0, 0)));
+    }
+    EXPECT_EQ(aC.TShape()->NbChildren(), 50);
+  }
+
+  // Bucket size larger than needed
+  {
+    TopoDS_Compound aC;
+    aBuilder.MakeCompound(aC, 1000);
+    for (int i = 0; i < 10; ++i)
+    {
+      aBuilder.Add(aC, BRepBuilderAPI_MakeVertex(gp_Pnt(i, 0, 0)));
+    }
+    EXPECT_EQ(aC.TShape()->NbChildren(), 10);
+  }
+}
+
+//=================================================================================================
+// Test all shape types with default bucket size
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, AllTypes_DefaultBucket)
+{
+  TopoDS_Builder aBuilder;
+
+  // Wire
+  TopoDS_Wire aWire;
+  aBuilder.MakeWire(aWire);
+  EXPECT_FALSE(aWire.IsNull());
+  EXPECT_EQ(aWire.ShapeType(), TopAbs_WIRE);
+  EXPECT_TRUE(aWire.Free());
+
+  // Shell
+  TopoDS_Shell aShell;
+  aBuilder.MakeShell(aShell);
+  EXPECT_FALSE(aShell.IsNull());
+  EXPECT_EQ(aShell.ShapeType(), TopAbs_SHELL);
+  EXPECT_TRUE(aShell.Free());
+
+  // Solid
+  TopoDS_Solid aSolid;
+  aBuilder.MakeSolid(aSolid);
+  EXPECT_FALSE(aSolid.IsNull());
+  EXPECT_EQ(aSolid.ShapeType(), TopAbs_SOLID);
+  EXPECT_TRUE(aSolid.Free());
+
+  // CompSolid
+  TopoDS_CompSolid aCompSolid;
+  aBuilder.MakeCompSolid(aCompSolid);
+  EXPECT_FALSE(aCompSolid.IsNull());
+  EXPECT_EQ(aCompSolid.ShapeType(), TopAbs_COMPSOLID);
+  EXPECT_TRUE(aCompSolid.Free());
+
+  // Compound
+  TopoDS_Compound aCompound;
+  aBuilder.MakeCompound(aCompound);
+  EXPECT_FALSE(aCompound.IsNull());
+  EXPECT_EQ(aCompound.ShapeType(), TopAbs_COMPOUND);
+  EXPECT_TRUE(aCompound.Free());
+}
+
+//=================================================================================================
+// Test all shape types with explicit bucket size of 1
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, AllTypes_BucketSize1)
+{
+  TopoDS_Builder aBuilder;
+
+  TopoDS_Wire aWire;
+  aBuilder.MakeWire(aWire, 1);
+  EXPECT_FALSE(aWire.IsNull());
+
+  TopoDS_Shell aShell;
+  aBuilder.MakeShell(aShell, 1);
+  EXPECT_FALSE(aShell.IsNull());
+
+  TopoDS_Solid aSolid;
+  aBuilder.MakeSolid(aSolid, 1);
+  EXPECT_FALSE(aSolid.IsNull());
+
+  TopoDS_CompSolid aCompSolid;
+  aBuilder.MakeCompSolid(aCompSolid, 1);
+  EXPECT_FALSE(aCompSolid.IsNull());
+
+  TopoDS_Compound aCompound;
+  aBuilder.MakeCompound(aCompound, 1);
+  EXPECT_FALSE(aCompound.IsNull());
+}
+
+//=================================================================================================
+// Test all shape types with large bucket size
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, AllTypes_LargeBucketSize)
+{
+  TopoDS_Builder aBuilder;
+
+  TopoDS_Wire aWire;
+  aBuilder.MakeWire(aWire, 10000);
+  EXPECT_FALSE(aWire.IsNull());
+
+  TopoDS_Shell aShell;
+  aBuilder.MakeShell(aShell, 10000);
+  EXPECT_FALSE(aShell.IsNull());
+
+  TopoDS_Solid aSolid;
+  aBuilder.MakeSolid(aSolid, 10000);
+  EXPECT_FALSE(aSolid.IsNull());
+
+  TopoDS_CompSolid aCompSolid;
+  aBuilder.MakeCompSolid(aCompSolid, 10000);
+  EXPECT_FALSE(aCompSolid.IsNull());
+
+  TopoDS_Compound aCompound;
+  aBuilder.MakeCompound(aCompound, 10000);
+  EXPECT_FALSE(aCompound.IsNull());
+}
+
+//=================================================================================================
+// Test mixed shapes in compound
+//=================================================================================================
+
+TEST(TopoDS_Builder_Test, MixedShapesInCompound)
+{
+  TopoDS_Builder  aBuilder;
+  TopoDS_Compound aCompound;
+  aBuilder.MakeCompound(aCompound, 10);
+
+  // Add different shape types
+  aBuilder.Add(aCompound, BRepBuilderAPI_MakeVertex(gp_Pnt(0, 0, 0)));
+  aBuilder.Add(aCompound, BRepBuilderAPI_MakeEdge(gp_Pnt(0, 0, 0), gp_Pnt(1, 0, 0)));
+
+  TopoDS_Wire aWire;
+  aBuilder.MakeWire(aWire);
+  aBuilder.Add(aCompound, aWire);
+
+  TopoDS_Shell aShell;
+  aBuilder.MakeShell(aShell);
+  aBuilder.Add(aCompound, aShell);
+
+  TopoDS_Compound aInner;
+  aBuilder.MakeCompound(aInner);
+  aBuilder.Add(aCompound, aInner);
+
+  EXPECT_EQ(aCompound.TShape()->NbChildren(), 5);
+
+  // Verify types via iteration
+  int aVertexCount = 0, aEdgeCount = 0, aWireCount = 0, aShellCount = 0, aCompoundCount = 0;
+  for (TopoDS_Iterator anIt(aCompound); anIt.More(); anIt.Next())
+  {
+    switch (anIt.Value().ShapeType())
+    {
+      case TopAbs_VERTEX:
+        ++aVertexCount;
+        break;
+      case TopAbs_EDGE:
+        ++aEdgeCount;
+        break;
+      case TopAbs_WIRE:
+        ++aWireCount;
+        break;
+      case TopAbs_SHELL:
+        ++aShellCount;
+        break;
+      case TopAbs_COMPOUND:
+        ++aCompoundCount;
+        break;
+      default:
+        break;
+    }
+  }
+
+  EXPECT_EQ(aVertexCount, 1);
+  EXPECT_EQ(aEdgeCount, 1);
+  EXPECT_EQ(aWireCount, 1);
+  EXPECT_EQ(aShellCount, 1);
+  EXPECT_EQ(aCompoundCount, 1);
+}
