bbfc7b7a62f15312b48bbc617be98651d1f8c5f4
dpasukhi <dpasukhi@opencascade.com>
2026-02-14 09:33:51 +0000

    Add Laguerre polynomial solver implementation and corresponding tests in MathPoly; enhance root-finding capabilities

--- src/FoundationClasses/TKMath/GTests/FILES.cmake
+++ src/FoundationClasses/TKMath/GTests/FILES.cmake
@@ -80,6 +80,7 @@ set(OCCT_TKMath_GTests_FILES
   # MathPoly tests
   MathPoly_Test.cxx
   MathPoly_Comparison_Test.cxx
+  MathPoly_Laguerre_Test.cxx
   # MathLin tests
   MathLin_Test.cxx
   MathLin_Comparison_Test.cxx
--- src/FoundationClasses/TKMath/MathPoly/FILES.cmake
+++ src/FoundationClasses/TKMath/MathPoly/FILES.cmake
@@ -5,4 +5,5 @@ set(OCCT_MathPoly_FILES
   MathPoly_Quadratic.hxx
   MathPoly_Cubic.hxx
   MathPoly_Quartic.hxx
+  MathPoly_Laguerre.hxx
 )
