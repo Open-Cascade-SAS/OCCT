4c5a8df587a48fdfe751a56c1dc7ccc705884dcc
dpasukhi <dpasukhi@opencascade.com>
2026-02-25 10:58:15 +0000

    returned old

--- src/ModelingData/TKGeomBase/AppCont/FILES.cmake
+++ src/ModelingData/TKGeomBase/AppCont/FILES.cmake
@@ -2,10 +2,12 @@
 set(OCCT_AppCont_FILES_LOCATION "${CMAKE_CURRENT_LIST_DIR}")
 
 set(OCCT_AppCont_FILES
+  AppCont_ContMatrices_BB.pxx
   AppCont_ContMatrices.pxx
   AppCont_ContMatrices_IBP.pxx
   AppCont_ContMatrices_IBT.pxx
   AppCont_ContMatrices_InvM.pxx
+  AppCont_ContMatrices_VB.pxx
   AppCont_Function.hxx
   AppCont_LeastSquare.cxx
   AppCont_LeastSquare.hxx
