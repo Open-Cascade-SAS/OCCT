325de104647b04b6220ba92ac338a8ae2291d862
dpasukhi <dpasukhi@opencascade.com>
2026-04-21 21:44:38 +0100

    //formatting

--- src/Visualization/TKV3d/GTests/Select3D_SensitivePrimitiveArray_Test.cxx
+++ src/Visualization/TKV3d/GTests/Select3D_SensitivePrimitiveArray_Test.cxx
@@ -27,7 +27,8 @@ TEST(Select3D_SensitivePrimitiveArrayTest, GetVertex_ReturnsTriangleVertices)
   aTris->AddVertex(0.0, 1.0, 0.0);
 
   occ::handle<SelectMgr_EntityOwner>            anOwner = new SelectMgr_EntityOwner();
-  occ::handle<Select3D_SensitivePrimitiveArray> aSens = new Select3D_SensitivePrimitiveArray(anOwner);
+  occ::handle<Select3D_SensitivePrimitiveArray> aSens =
+    new Select3D_SensitivePrimitiveArray(anOwner);
   ASSERT_TRUE(aSens->InitTriangulation(aTris->Attributes(), aTris->Indices(), TopLoc_Location()));
 
   const std::array<NCollection_Vec3<float>, 3> aVertices = aSens->GetVertex(0);
