5591896e653524f5ab264c82bc066d93b5828b97
dkulikov <dkulikov@opencascade.com>
2026-06-15 16:36:34 +0100

    Fix broken tests

--- src/ModelingAlgorithms/TKBO/BOPTools/BOPTools_AlgoTools.cxx
+++ src/ModelingAlgorithms/TKBO/BOPTools/BOPTools_AlgoTools.cxx
@@ -1072,12 +1072,12 @@ bool BOPTools_AlgoTools::GetFaceOff(const TopoDS_Edge&                        th
       {
         aAngle = aTwoPI;
       }
-      else
+      else if (!bIsComputed)
       {
-        // Near-zero angle between different faces (coplanar due to near-degenerate geometry,
-        // e.g. very thin box creating nearly-coincident intersection curves).
-        // Treat as maximum angle (2*PI) to prevent incorrect selection of a coplanar
-        // continuation face over a proper closing face.
+        // Near-zero angle between different faces where the bi-normal direction could not be
+        // reliably computed (near-degenerate geometry, e.g. very thin face producing
+        // nearly-coincident intersection curves). Treat as maximum angle (2*PI) to prevent
+        // incorrect selection of this unreliable face over a proper closing face.
         aAngle = aTwoPI;
       }
     }
