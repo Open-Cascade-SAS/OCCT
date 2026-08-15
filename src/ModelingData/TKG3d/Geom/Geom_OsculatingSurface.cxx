ac3dbcaf7811ee2d5ad050d3c2701555be7e7e73
dpasukhi <dpasukhi@opencascade.com>
2026-08-15 18:13:34 +0100

    // formatting

--- src/ModelingData/TKG3d/Geom/Geom_OsculatingSurface.cxx
+++ src/ModelingData/TKG3d/Geom/Geom_OsculatingSurface.cxx
@@ -36,7 +36,8 @@ class OsculatingSurfaceInitGuard
 {
 public:
   OsculatingSurfaceInitGuard(std::array<bool, 4>& theFlags, bool& theIsDone)
-      : myFlags(theFlags), myIsDone(theIsDone)
+      : myFlags(theFlags),
+        myIsDone(theIsDone)
   {
   }
 
@@ -50,7 +51,7 @@ public:
 
 private:
   std::array<bool, 4>& myFlags;
-  bool&                 myIsDone;
+  bool&                myIsDone;
 };
 } // namespace
 
@@ -129,7 +130,7 @@ Geom_OsculatingSurface& Geom_OsculatingSurface::operator=(
 void Geom_OsculatingSurface::Init(const occ::handle<Geom_Surface>& theBS, double theTol)
 {
   myAlong.fill(false);
-  bool aIsDone = false;
+  bool                       aIsDone = false;
   OsculatingSurfaceInitGuard aInitGuard(myAlong, aIsDone);
 
   myTol = theTol;
@@ -212,15 +213,13 @@ void Geom_OsculatingSurface::Init(const occ::handle<Geom_Surface>& theBS, double
   }
 
   // The reduction needs at least one direction to have degree > 1.
-  if ((IsAlongU() && aInitSurf->VDegree() <= 1)
-      || (IsAlongV() && aInitSurf->UDegree() <= 1))
+  if ((IsAlongU() && aInitSurf->VDegree() <= 1) || (IsAlongV() && aInitSurf->UDegree() <= 1))
   {
     return;
   }
 
-  const size_t aNbSurfaces = IsAlongU()
-                               ? static_cast<size_t>(aInitSurf->NbUKnots() - 1)
-                               : static_cast<size_t>(aInitSurf->NbVKnots() - 1);
+  const size_t aNbSurfaces = IsAlongU() ? static_cast<size_t>(aInitSurf->NbUKnots() - 1)
+                                        : static_cast<size_t>(aInitSurf->NbVKnots() - 1);
   myOsculSurf1.Reserve(aNbSurfaces);
   myOsculSurf2.Reserve(aNbSurfaces);
 
@@ -233,13 +232,7 @@ void Geom_OsculatingSurface::Init(const occ::handle<Geom_Surface>& theBS, double
       size_t                           aNbReductions = 0;
       if (myAlong[0])
       {
-        if (!reduceAlongIso(aInitSurf,
-                            aV1,
-                            GeomAbs_IsoV,
-                            i,
-                            1,
-                            aSurface,
-                            aNbReductions))
+        if (!reduceAlongIso(aInitSurf, aV1, GeomAbs_IsoV, i, 1, aSurface, aNbReductions))
         {
           return;
         }
@@ -270,13 +263,7 @@ void Geom_OsculatingSurface::Init(const occ::handle<Geom_Surface>& theBS, double
       size_t                           aNbReductions = 0;
       if (myAlong[2])
       {
-        if (!reduceAlongIso(aInitSurf,
-                            aU1,
-                            GeomAbs_IsoU,
-                            1,
-                            i,
-                            aSurface,
-                            aNbReductions))
+        if (!reduceAlongIso(aInitSurf, aU1, GeomAbs_IsoU, 1, i, aSurface, aNbReductions))
         {
           return;
         }
@@ -304,22 +291,21 @@ void Geom_OsculatingSurface::Init(const occ::handle<Geom_Surface>& theBS, double
 
 //=================================================================================================
 
-bool Geom_OsculatingSurface::reduceAlongIso(
-  const occ::handle<Geom_BSplineSurface>& theInitSurf,
-  double                                  theParam,
-  GeomAbs_IsoType                         theIso,
-  int                                     theInitUKnot,
-  int                                     theInitVKnot,
-  occ::handle<Geom_BSplineSurface>&       theSurface,
-  size_t&                                  theNbReductions) const
+bool Geom_OsculatingSurface::reduceAlongIso(const occ::handle<Geom_BSplineSurface>& theInitSurf,
+                                            double                                  theParam,
+                                            GeomAbs_IsoType                         theIso,
+                                            int                                     theInitUKnot,
+                                            int                                     theInitVKnot,
+                                            occ::handle<Geom_BSplineSurface>&       theSurface,
+                                            size_t& theNbReductions) const
 {
   theSurface.Nullify();
   theNbReductions = 0;
 
   occ::handle<Geom_BSplineSurface> aSurface = theInitSurf;
   occ::handle<Geom_BSplineSurface> aReducedSurface;
-  int                              aUKnot = theInitUKnot;
-  int                              aVKnot = theInitVKnot;
+  int                              aUKnot       = theInitUKnot;
+  int                              aVKnot       = theInitVKnot;
   bool                             aIsQPunctual = true;
   GeomAdaptor_Surface              anAdaptor;
   while (aIsQPunctual)
@@ -332,9 +318,9 @@ bool Geom_OsculatingSurface::reduceAlongIso(
     ++theNbReductions;
     anAdaptor.Load(aReducedSurface);
     aIsQPunctual = isQPunctual(anAdaptor, theParam, theIso, 0.0, myTol);
-    aUKnot        = 1;
-    aVKnot        = 1;
-    aSurface      = aReducedSurface;
+    aUKnot       = 1;
+    aVKnot       = 1;
+    aSurface     = aReducedSurface;
   }
 
   theSurface = aSurface;
@@ -345,10 +331,10 @@ bool Geom_OsculatingSurface::reduceAlongIso(
 
 void Geom_OsculatingSurface::appendOsculatingSurface2(
   const occ::handle<Geom_BSplineSurface>& theSurface,
-  size_t                                   theNbReductions)
+  size_t                                  theNbReductions)
 {
   OsculatingSurfaceData anOsculatingSurface;
-  anOsculatingSurface.Surface     = theSurface;
+  anOsculatingSurface.Surface    = theSurface;
   anOsculatingSurface.IsOpposite = (theNbReductions % 2) != 0;
   myOsculSurf2.Append(anOsculatingSurface);
 }
@@ -365,7 +351,7 @@ bool Geom_OsculatingSurface::UOsculatingSurface(double                          
     return false;
   }
 
-  theT = false;
+  theT                 = false;
   int  aNU             = 1;
   int  aNV             = 1;
   int  aNbUK           = 2;
@@ -373,12 +359,11 @@ bool Geom_OsculatingSurface::UOsculatingSurface(double                          
   bool aIsToSkipSecond = false;
   if (myBasisSurf->IsKind(STANDARD_TYPE(Geom_BSplineSurface)))
   {
-    const occ::handle<Geom_BSplineSurface> aBSur =
-      occ::down_cast<Geom_BSplineSurface>(myBasisSurf);
-    aNbUK                                      = aBSur->NbUKnots();
-    aNbVK                                      = aBSur->NbVKnots();
-    const NCollection_Array1<double>& aUKnots = aBSur->UKnots();
-    const NCollection_Array1<double>& aVKnots = aBSur->VKnots();
+    const occ::handle<Geom_BSplineSurface> aBSur = occ::down_cast<Geom_BSplineSurface>(myBasisSurf);
+    aNbUK                                        = aBSur->NbUKnots();
+    aNbVK                                        = aBSur->NbVKnots();
+    const NCollection_Array1<double>& aUKnots    = aBSur->UKnots();
+    const NCollection_Array1<double>& aVKnots    = aBSur->VKnots();
     BSplCLib::Hunt(aUKnots, theU, aNU);
     BSplCLib::Hunt(aVKnots, theV, aNV);
     aNU = std::clamp(aNU, 1, aNbUK - 1);
@@ -395,7 +380,7 @@ bool Geom_OsculatingSurface::UOsculatingSurface(double                          
   bool aAlong = false;
   if (myAlong[0] && aNV == 1)
   {
-    theL  = myOsculSurf1.Value(static_cast<size_t>(aNU - 1));
+    theL   = myOsculSurf1.Value(static_cast<size_t>(aNU - 1));
     aAlong = true;
   }
   if (myAlong[1] && aNV == aNbVK - 1 && !aIsToSkipSecond)
@@ -403,8 +388,8 @@ bool Geom_OsculatingSurface::UOsculatingSurface(double                          
     // The derivative is reversed when the number of reductions is odd.
     const OsculatingSurfaceData& anOsculatingSurface =
       myOsculSurf2.Value(static_cast<size_t>(aNU - 1));
-    theT = anOsculatingSurface.IsOpposite;
-    theL = anOsculatingSurface.Surface;
+    theT   = anOsculatingSurface.IsOpposite;
+    theL   = anOsculatingSurface.Surface;
     aAlong = true;
   }
   return aAlong;
@@ -422,7 +407,7 @@ bool Geom_OsculatingSurface::VOsculatingSurface(double                          
     return false;
   }
 
-  theT = false;
+  theT                 = false;
   int  aNU             = 1;
   int  aNV             = 1;
   int  aNbUK           = 2;
@@ -430,12 +415,11 @@ bool Geom_OsculatingSurface::VOsculatingSurface(double                          
   bool aIsToSkipSecond = false;
   if (myBasisSurf->IsKind(STANDARD_TYPE(Geom_BSplineSurface)))
   {
-    const occ::handle<Geom_BSplineSurface> aBSur =
-      occ::down_cast<Geom_BSplineSurface>(myBasisSurf);
-    aNbUK                                      = aBSur->NbUKnots();
-    aNbVK                                      = aBSur->NbVKnots();
-    const NCollection_Array1<double>& aUKnots = aBSur->UKnots();
-    const NCollection_Array1<double>& aVKnots = aBSur->VKnots();
+    const occ::handle<Geom_BSplineSurface> aBSur = occ::down_cast<Geom_BSplineSurface>(myBasisSurf);
+    aNbUK                                        = aBSur->NbUKnots();
+    aNbVK                                        = aBSur->NbVKnots();
+    const NCollection_Array1<double>& aUKnots    = aBSur->UKnots();
+    const NCollection_Array1<double>& aVKnots    = aBSur->VKnots();
     BSplCLib::Hunt(aUKnots, theU, aNU);
     BSplCLib::Hunt(aVKnots, theV, aNV);
     aNV = std::clamp(aNV, 1, aNbVK - 1);
@@ -452,15 +436,15 @@ bool Geom_OsculatingSurface::VOsculatingSurface(double                          
   bool aAlong = false;
   if (myAlong[2] && aNU == 1)
   {
-    theL  = myOsculSurf1.Value(static_cast<size_t>(aNV - 1));
+    theL   = myOsculSurf1.Value(static_cast<size_t>(aNV - 1));
     aAlong = true;
   }
   if (myAlong[3] && aNU == aNbUK - 1 && !aIsToSkipSecond)
   {
     const OsculatingSurfaceData& anOsculatingSurface =
       myOsculSurf2.Value(static_cast<size_t>(aNV - 1));
-    theT = anOsculatingSurface.IsOpposite;
-    theL = anOsculatingSurface.Surface;
+    theT   = anOsculatingSurface.IsOpposite;
+    theL   = anOsculatingSurface.Surface;
     aAlong = true;
   }
   return aAlong;
@@ -468,12 +452,11 @@ bool Geom_OsculatingSurface::VOsculatingSurface(double                          
 
 //=================================================================================================
 
-bool Geom_OsculatingSurface::buildOsculatingSurface(
-  double                                  theParam,
-  int                                     theSUKnot,
-  int                                     theSVKnot,
-  const occ::handle<Geom_BSplineSurface>& theBS,
-  occ::handle<Geom_BSplineSurface>&       theBSpl) const
+bool Geom_OsculatingSurface::buildOsculatingSurface(double theParam,
+                                                    int    theSUKnot,
+                                                    int    theSVKnot,
+                                                    const occ::handle<Geom_BSplineSurface>& theBS,
+                                                    occ::handle<Geom_BSplineSurface>& theBSpl) const
 {
   const int aUDeg = theBS->UDegree();
   const int aVDeg = theBS->VDegree();
@@ -492,12 +475,11 @@ bool Geom_OsculatingSurface::buildOsculatingSurface(
   NCollection_LocalArray<double, 2> aPolyVStorage(2);
   NCollection_LocalArray<double, 2> aTrueUStorage(2);
   NCollection_LocalArray<double, 2> aTrueVStorage(2);
-  NCollection_Array2<int>           aNumCoeffPerSurface(
-    aNumCoeffStorage.begin()[0], 1, 1, 1, 2);
-  NCollection_Array1<double> aPolyUIntervals(aPolyUStorage.begin()[0], 1, 2);
-  NCollection_Array1<double> aPolyVIntervals(aPolyVStorage.begin()[0], 1, 2);
-  NCollection_Array1<double> aTrueUIntervals(aTrueUStorage.begin()[0], 1, 2);
-  NCollection_Array1<double> aTrueVIntervals(aTrueVStorage.begin()[0], 1, 2);
+  NCollection_Array2<int>           aNumCoeffPerSurface(aNumCoeffStorage.begin()[0], 1, 1, 1, 2);
+  NCollection_Array1<double>        aPolyUIntervals(aPolyUStorage.begin()[0], 1, 2);
+  NCollection_Array1<double>        aPolyVIntervals(aPolyVStorage.begin()[0], 1, 2);
+  NCollection_Array1<double>        aTrueUIntervals(aTrueUStorage.begin()[0], 1, 2);
+  NCollection_Array1<double>        aTrueVIntervals(aTrueVStorage.begin()[0], 1, 2);
 
   for (size_t i = 0; i < 2; ++i)
   {
@@ -521,14 +503,14 @@ bool Geom_OsculatingSurface::buildOsculatingSurface(
   }
   aNumCoeffPerSurface.ChangeAt(0, 0) = aOscUNumCoeff;
   aNumCoeffPerSurface.ChangeAt(0, 1) = aOscVNumCoeff;
-  const int aNbCoef = aOscUNumCoeff * aOscVNumCoeff * 3;
+  const int aNbCoef                  = aOscUNumCoeff * aOscVNumCoeff * 3;
   if (aNbCoef == 0)
   {
     return false;
   }
 
   NCollection_LocalArray<double> aCoefficientStorage(static_cast<size_t>(aNbCoef));
-  NCollection_Array1<double>      aCoefficients(aCoefficientStorage.begin()[0], 1, aNbCoef);
+  NCollection_Array1<double>     aCoefficients(aCoefficientStorage.begin()[0], 1, aNbCoef);
 
   const NCollection_Array2<gp_Pnt>& aPoles     = theBS->Poles();
   const NCollection_Array1<double>& aUFlatKnts = theBS->UKnotSequence();
@@ -605,7 +587,7 @@ bool Geom_OsculatingSurface::buildOsculatingSurface(
     {
       for (size_t m = 0; m < aVNbPoles - 1; ++m)
       {
-        const gp_Pnt& aPole = aOscCoeff.At(n, m);
+        const gp_Pnt& aPole            = aOscCoeff.At(n, m);
         aCoefficients.ChangeAt(aIdx++) = aPole.X();
         aCoefficients.ChangeAt(aIdx++) = aPole.Y();
         aCoefficients.ChangeAt(aIdx++) = aPole.Z();
@@ -644,7 +626,7 @@ bool Geom_OsculatingSurface::buildOsculatingSurface(
     {
       for (size_t m = 0; m < aVNbPoles; ++m)
       {
-        const gp_Pnt& aPole = aOscCoeff.At(n, m);
+        const gp_Pnt& aPole            = aOscCoeff.At(n, m);
         aCoefficients.ChangeAt(aIdx++) = aPole.X();
         aCoefficients.ChangeAt(aIdx++) = aPole.Y();
         aCoefficients.ChangeAt(aIdx++) = aPole.Z();
@@ -683,23 +665,23 @@ bool Geom_OsculatingSurface::buildOsculatingSurface(
 //=================================================================================================
 
 bool Geom_OsculatingSurface::isQPunctual(const GeomAdaptor_Surface& theAdaptor,
-                                         double                      theParam,
-                                         GeomAbs_IsoType             theIT,
-                                         double                      theTolMin,
-                                         double                      theTolMax) const
+                                         double                     theParam,
+                                         GeomAbs_IsoType            theIT,
+                                         double                     theTolMin,
+                                         double                     theTolMax) const
 {
   const bool   anIsIsoV = theIT == GeomAbs_IsoV;
   const double aPmin    = anIsIsoV ? theAdaptor.FirstUParameter() : theAdaptor.FirstVParameter();
   const double aPmax    = anIsIsoV ? theAdaptor.LastUParameter() : theAdaptor.LastVParameter();
-  const int    aNbIntervals = anIsIsoV ? theAdaptor.NbUIntervals(GeomAbs_CN)
-                                      : theAdaptor.NbVIntervals(GeomAbs_CN);
+  const int    aNbIntervals =
+    anIsIsoV ? theAdaptor.NbUIntervals(GeomAbs_CN) : theAdaptor.NbVIntervals(GeomAbs_CN);
   if (aNbIntervals < 1)
   {
     return false;
   }
 
   NCollection_LocalArray<double> aIntervalStorage(static_cast<size_t>(aNbIntervals + 1));
-  NCollection_Array1<double>      aIntervals(aIntervalStorage.begin()[0], 1, aNbIntervals + 1);
+  NCollection_Array1<double>     aIntervals(aIntervalStorage.begin()[0], 1, aNbIntervals + 1);
   if (anIsIsoV)
   {
     theAdaptor.UIntervals(aIntervals, GeomAbs_CN);
@@ -708,23 +690,22 @@ bool Geom_OsculatingSurface::isQPunctual(const GeomAdaptor_Surface& theAdaptor,
   {
     theAdaptor.VIntervals(aIntervals, GeomAbs_CN);
   }
-  aIntervals.ChangeAt(0) = aPmin;
+  aIntervals.ChangeAt(0)                                 = aPmin;
   aIntervals.ChangeAt(static_cast<size_t>(aNbIntervals)) = aPmax;
 
-  const int aDegree = anIsIsoV ? theAdaptor.UDegree() : theAdaptor.VDegree();
-  const size_t aNbSamples = static_cast<size_t>(std::max(aDegree, 1));
-  double       aD1NormMax = 0.0;
-  auto updateD1Norm = [&](const double theParameter)
-  {
+  const int    aDegree      = anIsIsoV ? theAdaptor.UDegree() : theAdaptor.VDegree();
+  const size_t aNbSamples   = static_cast<size_t>(std::max(aDegree, 1));
+  double       aD1NormMax   = 0.0;
+  auto         updateD1Norm = [&](const double theParameter) {
     if (anIsIsoV)
     {
       const Geom_Surface::ResD1 aResult = theAdaptor.EvalD1(theParameter, theParam);
-      aD1NormMax = std::max(aD1NormMax, aResult.D1U.Magnitude());
+      aD1NormMax                        = std::max(aD1NormMax, aResult.D1U.Magnitude());
     }
     else
     {
       const Geom_Surface::ResD1 aResult = theAdaptor.EvalD1(theParam, theParameter);
-      aD1NormMax = std::max(aD1NormMax, aResult.D1V.Magnitude());
+      aD1NormMax                        = std::max(aD1NormMax, aResult.D1V.Magnitude());
     }
   };
 
@@ -735,10 +716,10 @@ bool Geom_OsculatingSurface::isQPunctual(const GeomAdaptor_Surface& theAdaptor,
     const double aRange = aLast - aFirst;
     for (size_t anIndex = 0; anIndex <= aNbSamples; ++anIndex)
     {
-      const double aParameter = anIndex == aNbSamples
-                                  ? aLast
-                                  : aFirst + aRange * static_cast<double>(anIndex)
-                                               / static_cast<double>(aNbSamples);
+      const double aParameter =
+        anIndex == aNbSamples
+          ? aLast
+          : aFirst + aRange * static_cast<double>(anIndex) / static_cast<double>(aNbSamples);
       updateD1Norm(aParameter);
     }
   }
