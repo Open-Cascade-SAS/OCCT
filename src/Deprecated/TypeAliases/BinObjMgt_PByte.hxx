20f7fda60e64713455d3a5d887759fc1792147e2
dpasukhi <dpasukhi@opencascade.com>
2026-08-18 15:37:33 +0100

    Coding - Restore deprecated type alias headers
    - Restore compatibility headers for removed typedef-only declarations.
    - Mark restored headers and aliases deprecated since OCCT 8.1.0.
    - Group legacy typedefs under Deprecated/TypeAliases.
    - Keep aliases of fully removed classes excluded.

---
+++ src/Deprecated/TypeAliases/BinObjMgt_PByte.hxx
@@ -0,0 +1,33 @@
+// Created on: 2007-08-10
+// Created by: Vlad ROMASHKO
+// Copyright (c) 2007-2014 OPEN CASCADE SAS
+//
+// This file is part of Open CASCADE Technology software library.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License version 2.1 as published
+// by the Free Software Foundation, with special exception defined in the file
+// OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
+// distribution for complete text of the license and disclaimer of any warranty.
+//
+// Alternatively, this file may be used under the terms of Open CASCADE
+// commercial license or contractual agreement.
+
+//! @file BinObjMgt_PByte.hxx
+//! @brief Deprecated typedef for backward compatibility.
+//! @deprecated This header is deprecated since OCCT 8.1.0.
+//!             Use the underlying type directly instead.
+
+#ifndef BinObjMgt_PByte_HeaderFile
+#define BinObjMgt_PByte_HeaderFile
+
+#include <Standard_Macro.hxx>
+#include <Standard_TypeDef.hxx>
+
+Standard_HEADER_DEPRECATED("BinObjMgt_PByte.hxx is deprecated since OCCT 8.1.0. "
+                           "Use the underlying type directly instead.")
+
+  Standard_DEPRECATED("BinObjMgt_PByte is deprecated, use the underlying type directly")
+typedef uint8_t* BinObjMgt_PByte;
+
+#endif // BinObjMgt_PByte_HeaderFile
