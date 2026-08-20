65865916f707031f1c1080f3ac185195f2c2a1e9
dpasukhi <dpasukhi@opencascade.com>
2026-08-20 20:27:44 +0100

    Modeling Data - Unify Adaptor2d curve evaluation API
      - Make Value(), D0-D3(), and DN() non-virtual wrappers delegating to EvalD*().
      - Move evaluation logic to EvalD*() in Adaptor2d_Line2d and Adaptor2d_OffsetCurve.
      - Remove redundant legacy evaluation overrides from Geom2dAdaptor_Curve.
      - Migrate ProjLib_CompProjectedCurve evaluation logic to EvalD*().
      - Remove unimplemented evaluation overrides from ProjLib_ProjectedCurve.
      - Update related documentation for the Eval-based API.

--- src/ModelingData/TKG2d/Adaptor2d/Adaptor2d_Curve2d.hxx
+++ src/ModelingData/TKG2d/Adaptor2d/Adaptor2d_Curve2d.hxx
@@ -85,39 +85,57 @@ public:
   Standard_EXPORT virtual double Period() const;
 
   //! Computes the point of parameter U on the curve.
-  Standard_EXPORT virtual gp_Pnt2d Value(const double U) const;
+  gp_Pnt2d Value(const double theU) const { return EvalD0(theU); }
 
   //! Computes the point of parameter U on the curve.
-  Standard_EXPORT virtual void D0(const double U, gp_Pnt2d& P) const;
+  void D0(const double theU, gp_Pnt2d& theP) const { theP = EvalD0(theU); }
 
   //! Computes the point of parameter U on the curve with its
   //! first derivative.
   //! Raised if the continuity of the current interval
   //! is not C1.
-  Standard_EXPORT virtual void D1(const double U, gp_Pnt2d& P, gp_Vec2d& V) const;
+  void D1(const double theU, gp_Pnt2d& theP, gp_Vec2d& theV) const
+  {
+    const Geom2d_Curve::ResD1 aRes = EvalD1(theU);
+    theP                           = aRes.Point;
+    theV                           = aRes.D1;
+  }
 
   //! Returns the point P of parameter U, the first and second
   //! derivatives V1 and V2.
   //! Raised if the continuity of the current interval
   //! is not C2.
-  Standard_EXPORT virtual void D2(const double U, gp_Pnt2d& P, gp_Vec2d& V1, gp_Vec2d& V2) const;
+  void D2(const double theU, gp_Pnt2d& theP, gp_Vec2d& theV1, gp_Vec2d& theV2) const
+  {
+    const Geom2d_Curve::ResD2 aRes = EvalD2(theU);
+    theP                           = aRes.Point;
+    theV1                          = aRes.D1;
+    theV2                          = aRes.D2;
+  }
 
   //! Returns the point P of parameter U, the first, the second
   //! and the third derivative.
   //! Raised if the continuity of the current interval
   //! is not C3.
-  Standard_EXPORT virtual void D3(const double U,
-                                  gp_Pnt2d&    P,
-                                  gp_Vec2d&    V1,
-                                  gp_Vec2d&    V2,
-                                  gp_Vec2d&    V3) const;
+  void D3(const double theU,
+          gp_Pnt2d&    theP,
+          gp_Vec2d&    theV1,
+          gp_Vec2d&    theV2,
+          gp_Vec2d&    theV3) const
+  {
+    const Geom2d_Curve::ResD3 aRes = EvalD3(theU);
+    theP                           = aRes.Point;
+    theV1                          = aRes.D1;
+    theV2                          = aRes.D2;
+    theV3                          = aRes.D3;
+  }
 
   //! The returned vector gives the value of the derivative for the
   //! order of derivation N.
   //! Raised if the continuity of the current interval
   //! is not CN.
   //! Raised if N < 1.
-  Standard_EXPORT virtual gp_Vec2d DN(const double U, const int N) const;
+  gp_Vec2d DN(const double theU, const int theN) const { return EvalDN(theU, theN); }
 
   //! Returns the parametric resolution corresponding
   //! to the real space resolution <R3d>.
