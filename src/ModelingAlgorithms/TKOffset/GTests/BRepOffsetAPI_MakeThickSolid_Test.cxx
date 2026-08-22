67e5c673c685f88d9c44980244c1ffb066d96138
Angelo Bartolome <angelo.m.bartolome@gmail.com>
2026-08-22 16:11:28 -0300

    Add new tests for BRepOffsetAPI_MakeThickSolid and implement circular wire creation
    - Introduced a new function, MakeCircularWire, to create circular wires using specified center, normal, and radius.
    - Added a test case, LoftTwoCircles_ShellTopFace, to validate the creation of a lofted shape from two circular wires and ensure the integrity of the resulting thick solid.
    - Enhanced the test suite for BRepOffsetAPI_MakeThickSolid with additional checks for surface properties and validity of the generated shapes.

--- src/ModelingAlgorithms/TKOffset/GTests/BRepOffsetAPI_MakeThickSolid_Test.cxx
+++ src/ModelingAlgorithms/TKOffset/GTests/BRepOffsetAPI_MakeThickSolid_Test.cxx
@@ -11,20 +11,26 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
+#include <BRepBuilderAPI_MakeEdge.hxx>
+#include <BRepBuilderAPI_MakeWire.hxx>
 #include <BRepCheck_Analyzer.hxx>
 #include <BRepGProp.hxx>
 #include <BRepOffsetAPI_MakeThickSolid.hxx>
+#include <BRepOffsetAPI_ThruSections.hxx>
 #include <BRepPrimAPI_MakeBox.hxx>
 #include <GProp_GProps.hxx>
+#include <GeomAbs_JoinType.hxx>
 #include <NCollection_List.hxx>
 #include <Precision.hxx>
-#include <TopAbs_ShapeEnum.hxx>
 #include <TopExp_Explorer.hxx>
 #include <TopoDS.hxx>
 #include <TopoDS_Face.hxx>
 #include <TopoDS_Shape.hxx>
-
-#include <cmath>
+#include <TopoDS_Wire.hxx>
+#include <gp_Ax2.hxx>
+#include <gp_Circ.hxx>
+#include <gp_Dir.hxx>
+#include <gp_Pnt.hxx>
 
 #include <gtest/gtest.h>
 
@@ -146,4 +152,78 @@ TEST(BRepOffsetAPI_MakeThickSolidTest, ThickSolidLargerVolume)
   // The thickened face should produce a solid with volume > 0.
   // A planar face of area A thickened by offset d produces volume approximately A*d.
   EXPECT_GT(aResultVolume, aFaceArea * anOffset * 0.5);
+}
+
+static TopoDS_Wire MakeCircularWire(const gp_Pnt& theCenter,
+                                    const gp_Dir& theNormal,
+                                    const double  theRadius)
+{
+  BRepBuilderAPI_MakeEdge anEdgeMaker(gp_Circ(gp_Ax2(theCenter, theNormal), theRadius));
+  if (!anEdgeMaker.IsDone())
+  {
+    return TopoDS_Wire();
+  }
+  BRepBuilderAPI_MakeWire aWireMaker(anEdgeMaker.Edge());
+  return aWireMaker.IsDone() ? aWireMaker.Wire() : TopoDS_Wire();
+}
+
+TEST(BRepOffsetAPI_MakeThickSolidTest, LoftTwoCircles_ShellTopFace)
+{
+  BRepOffsetAPI_ThruSections aLoftMaker(true, false);
+  aLoftMaker.AddWire(MakeCircularWire(gp_Pnt(0.0, -65.0, 0.0), gp_Dir(0.0, 0.0, 1.0), 30.0));
+  aLoftMaker.AddWire(MakeCircularWire(gp_Pnt(0.0, 0.0, 70.0), gp_Dir(0.0, 1.0, 0.0), 15.0));
+  aLoftMaker.Build();
+  ASSERT_TRUE(aLoftMaker.IsDone());
+
+  const TopoDS_Shape& aLoft = aLoftMaker.Shape();
+
+  TopoDS_Face aOpening;
+  double      aMaxZ = -1.0e100;
+  for (TopExp_Explorer anExp(aLoft, TopAbs_FACE); anExp.More(); anExp.Next())
+  {
+    const TopoDS_Face aFace = TopoDS::Face(anExp.Current());
+    GProp_GProps      aProps;
+    BRepGProp::SurfaceProperties(aFace, aProps);
+    if (aProps.CentreOfMass().Z() > aMaxZ)
+    {
+      aMaxZ    = aProps.CentreOfMass().Z();
+      aOpening = aFace;
+    }
+  }
+  ASSERT_FALSE(aOpening.IsNull());
+
+  GProp_GProps aOpenProps;
+  BRepGProp::SurfaceProperties(aOpening, aOpenProps);
+
+  NCollection_List<TopoDS_Shape> aFacesToRemove;
+  aFacesToRemove.Append(aOpening);
+
+  BRepOffsetAPI_MakeThickSolid aMaker;
+  aMaker.MakeThickSolidByJoin(aLoft,
+                              aFacesToRemove,
+                              -0.8,
+                              Precision::Confusion(),
+                              BRepOffset_Skin,
+                              false,
+                              false,
+                              GeomAbs_Intersection);
+  aMaker.Build();
+  ASSERT_TRUE(aMaker.IsDone());
+
+  const TopoDS_Shape& aResult = aMaker.Shape();
+  ASSERT_FALSE(aResult.IsNull());
+  EXPECT_TRUE(BRepCheck_Analyzer(aResult).IsValid());
+
+  bool aOpeningKept = false;
+  for (TopExp_Explorer anExp(aResult, TopAbs_FACE); anExp.More(); anExp.Next())
+  {
+    GProp_GProps aProps;
+    BRepGProp::SurfaceProperties(TopoDS::Face(anExp.Current()), aProps);
+    if (aProps.CentreOfMass().Distance(aOpenProps.CentreOfMass()) < 1.0)
+    {
+      aOpeningKept = true;
+      break;
+    }
+  }
+  EXPECT_FALSE(aOpeningKept);
 }
